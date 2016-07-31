a7852e2d517553118f35a7ee4470c16f59630218
Bernardo Martínez Garrido <programming@wandrell.com>
2016-07-31 20:36:57 +0200

    Initial rank is updated when selecting the rank options.

---
+++ src/main/webapp/resources/js/build/dbx/sponsorEdit.js
@@ -0,0 +1,43 @@
+/**
+ * Licensed under the MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+/**
+ * Scripts for DBX team edition.
+ * 
+ * Mostly it takes care of AJAX calls.
+ */
+
+$(document).ready(function() {
+	$(document).on("change", "input:radio", function() {
+		refreshRank();
+	});
+});
+
+function increaseRank(increase) {
+	var rankInput = $('#initialRank');
+	var initialRank = rankInput.val();
+
+	if (increase) {
+		initialRank++;
+	} else if (initialRank > 0) {
+		initialRank--;
+	}
+
+	rankInput.val(initialRank);
+}
+
+function refreshRank() {
+	var selected = $('input:checked');
+	var rankInput = $('#initialRank');
+	var rankBonus;
+
+	rankBonus = 0;
+
+	$.each(selected, function(index, value) {
+		if ($(value).hasClass('increaseRank')) {
+			rankBonus++;
+		}
+	});
+
+	rankInput.val(5 + rankBonus);
+}
