6ae74935920fd111bacbdbf33130d9f68acc6bd0
Bernardo Martínez Garrido <programming@wandrell.com>
2016-08-07 14:43:08 +0200

    JSON dependencies are now handled through the XML configuration

--- src/main/java/com/wandrell/tabletop/dreadball/web/toolkit/json/DreadballObjectMapper.java
+++ src/main/java/com/wandrell/tabletop/dreadball/web/toolkit/json/DreadballObjectMapper.java
@@ -3,18 +3,13 @@ package com.wandrell.tabletop.dreadball.web.toolkit.json;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public final class DreadballObjectMapper extends ObjectMapper {
 
     private static final long serialVersionUID = 7809888101081207633L;
 
-    @Autowired
-    public DreadballObjectMapper(
-            @Qualifier("jsonModelModule") final JsonModelModule module) {
+    public DreadballObjectMapper(final JsonModelModule module) {
         super();
 
         checkNotNull(module, "Received a null pointer as JSON module");
