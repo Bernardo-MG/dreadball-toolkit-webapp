eb7c78bd9c7e17fa8e2973dd50e1bc7a8bd696f1
Bernardo Martínez Garrido <programming@wandrell.com>
2016-10-03 22:19:43 +0200

    Added tests for invalid data

---
+++ src/test/java/com/wandrell/tabletop/dreadball/web/toolkit/test/unit/controller/builder/dbx/TestDbxTeamBuilderRestControllerAddPlayersInvalid.java
@@ -0,0 +1,174 @@
+/**
+ * Copyright 2016 the original author or authors
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.wandrell.tabletop.dreadball.web.toolkit.test.unit.controller.builder.dbx;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+import org.mockito.Mockito;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wandrell.tabletop.dreadball.build.dbx.DbxTeamBuilder;
+import com.wandrell.tabletop.dreadball.model.faction.DefaultSponsor;
+import com.wandrell.tabletop.dreadball.model.team.DefaultSponsorTeam;
+import com.wandrell.tabletop.dreadball.model.team.calculator.RankCostCalculator;
+import com.wandrell.tabletop.dreadball.model.team.calculator.TeamValorationCalculator;
+import com.wandrell.tabletop.dreadball.model.unit.DefaultUnit;
+import com.wandrell.tabletop.dreadball.model.unit.Role;
+import com.wandrell.tabletop.dreadball.model.unit.Unit;
+import com.wandrell.tabletop.dreadball.model.unit.stats.Ability;
+import com.wandrell.tabletop.dreadball.model.unit.stats.MutableAttributes;
+import com.wandrell.tabletop.dreadball.web.toolkit.builder.dbx.controller.DbxTeamBuilderRestController;
+import com.wandrell.tabletop.dreadball.web.toolkit.builder.dbx.controller.bean.SponsorTeamPlayer;
+
+/**
+ * Unit tests for {@link DbxTeamBuilderRestController}, checking the methods for
+ * adding players with invalid data.
+ * 
+ * @author Bernardo Mart&iacute;nez Garrido
+ */
+public final class TestDbxTeamBuilderRestControllerAddPlayersInvalid {
+
+    /**
+     * The name of the team bean.
+     */
+    private static final String TEAM_BEAN  = "team";
+
+    /**
+     * Form view URL.
+     */
+    private static final String URL_ASSETS = "/builder/team/dbx/players";
+
+    /**
+     * Default constructor.
+     */
+    public TestDbxTeamBuilderRestControllerAddPlayersInvalid() {
+        super();
+    }
+
+    /**
+     * Tests that when adding multiple players these are added one after the
+     * other.
+     */
+    @Test
+    public final void testAddPlayer_MaximumPlayers_NotAdded() throws Exception {
+        final ResultActions result;     // Request result
+        final SponsorTeamPlayer player; // Assets for the team
+        final RequestBuilder post;      // Request
+        final MockMvc mockMvc;          // Mocked context
+
+        mockMvc = MockMvcBuilders.standaloneSetup(getMaxUnitsZeroController())
+                .build();
+
+        player = new SponsorTeamPlayer();
+
+        player.setTemplateName("template");
+
+        // The request is created
+        post = getValidRequest(player);
+
+        mockMvc.perform(post).andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(post).andExpect(MockMvcResultMatchers.status().isOk());
+        result = mockMvc.perform(post)
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        // The assets were set correctly
+        // TODO: Should check size
+        result.andExpect(
+                MockMvcResultMatchers.jsonPath("$.players.1").doesNotExist());
+    }
+
+    /**
+     * Returns a mocked controller.
+     * <p>
+     * It can create mocked sponsor, sponsor team and units.
+     * 
+     * @return a mocked controller
+     */
+    @SuppressWarnings("unchecked")
+    private final DbxTeamBuilderRestController getMaxUnitsZeroController() {
+        final DbxTeamBuilder builder;
+        final Unit unit;
+        final Integer maxUnits;
+
+        builder = Mockito.mock(DbxTeamBuilder.class);
+
+        // TODO: Mock this better
+        unit = new DefaultUnit("", 0, Role.GUARD, new MutableAttributes(),
+                new LinkedList<Ability>(), false, false);
+
+        Mockito.when(builder.getUnit(org.mockito.Matchers.anyString(),
+                org.mockito.Matchers.anyCollection())).thenReturn(unit);
+
+        maxUnits = 0;
+
+        Mockito.when(builder.getMaxTeamUnits()).thenReturn(maxUnits);
+
+        return new DbxTeamBuilderRestController(builder);
+    }
+
+    /**
+     * Returns the session attributes required for the controller to work.
+     * 
+     * @return the session attributes required by the controller
+     */
+    @SuppressWarnings("unchecked")
+    private final Map<String, Object> getSessionAttributes() {
+        final Map<String, Object> sessionAttrs;
+
+        sessionAttrs = new LinkedHashMap<>();
+        // sessionAttrs.put("team", Mockito.mock(SponsorTeam.class));
+        // TODO: Mock this better
+        sessionAttrs.put(TEAM_BEAN,
+                new DefaultSponsorTeam(new DefaultSponsor(),
+                        Mockito.mock(TeamValorationCalculator.class),
+                        Mockito.mock(RankCostCalculator.class)));
+
+        return sessionAttrs;
+    }
+
+    /**
+     * Returns a request builder for posting the specified assets.
+     * <p>
+     * The created request will contain the valid context.
+     * 
+     * @param player
+     *            player data for the request
+     * @return a request builder with the specified player data
+     */
+    private final RequestBuilder getValidRequest(final SponsorTeamPlayer player)
+            throws IOException {
+        final byte[] content;
+
+        content = new ObjectMapper().writeValueAsBytes(player);
+
+        return MockMvcRequestBuilders.post(URL_ASSETS)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .sessionAttrs(getSessionAttributes()).content(content);
+    }
+
+}
