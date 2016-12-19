934f4bbd98ec26334c839a8c2750cd55904f6fe0
Bernardo Martínez Garrido <programming@wandrell.com>
2016-12-19 21:29:05 +0100

    Extended the error bean

--- src/main/java/com/wandrell/tabletop/dreadball/web/toolkit/controller/bean/DefaultErrorResponse.java
+++ src/main/java/com/wandrell/tabletop/dreadball/web/toolkit/controller/bean/DefaultErrorResponse.java
@@ -3,9 +3,21 @@ package com.wandrell.tabletop.dreadball.web.toolkit.controller.bean;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "error")
+@XmlAccessorType(XmlAccessType.FIELD)
 public final class DefaultErrorResponse implements ErrorResponse {
 
-    private final String message;
+    @XmlElement(name = "message")
+    private String message;
+
+    public DefaultErrorResponse() {
+        super();
+    }
 
     public DefaultErrorResponse(final String message) {
         super();
@@ -17,6 +29,10 @@ public final class DefaultErrorResponse implements ErrorResponse {
     @Override
     public final String getMessage() {
         return message;
+    }
+
+    public final void setMessage(final String message) {
+        this.message = message;
     }
 
 }
