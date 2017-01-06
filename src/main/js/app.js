50a7dec67d9ef0d63a63e24f39d5c59a729e150b
Bernardo Martínez Garrido <programming@wandrell.com>
2017-01-06 17:10:36 +0100

    Begins applying CSS

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -4,23 +4,15 @@
 const React = require('react');
 const ReactDOM = require('react-dom')
 const Header = require('./template/header');
-const client = require('./client');
 // end::vars[]
 
-import s from '../css/main.scss';
+//import 'resources/css/style.scss';
 
 // tag::app[]
 class App extends React.Component {
 
         constructor(props) {
                 super(props);
-                this.state = {employees: []};
-        }
-
-        componentDidMount() {
-                client({method: 'GET', path: '/api/employees'}).done(response => {
-                        this.setState({employees: response.entity._embedded.employees});
-                });
         }
 
         render() {
@@ -30,42 +22,6 @@ class App extends React.Component {
         }
 }
 // end::app[]
-
-// tag::employee-list[]
-class EmployeeList extends React.Component{
-        render() {
-                var employees = this.props.employees.map(employee =>
-                        <Employee key={employee._links.self.href} employee={employee}/>
-                );
-                return (
-                        <table>
-                                <tbody>
-                                        <tr>
-                                                <th>First Name</th>
-                                                <th>Last Name</th>
-                                                <th>Description</th>
-                                        </tr>
-                                        {employees}
-                                </tbody>
-                        </table>
-                )
-        }
-}
-// end::employee-list[]
-
-// tag::employee[]
-class Employee extends React.Component{
-        render() {
-                return (
-                        <tr>
-                                <td>{this.props.employee.firstName}</td>
-                                <td>{this.props.employee.lastName}</td>
-                                <td>{this.props.employee.description}</td>
-                        </tr>
-                )
-        }
-}
-// end::employee[]
 
 // tag::render[]
 ReactDOM.render(
