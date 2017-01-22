8517f1855ada5c43b219dfb016dcb1c7d170c2b7
Bernardo <programming@wandrell.com>
2017-01-22 22:23:12 +0100

    Using react router

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -2,11 +2,13 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom')
+import { Router, Route, hashHistory } from 'react-router'
 import DreadballApp from './DreadballApp';
 
 require('./theme/style.scss');
 
-ReactDOM.render(
-        <DreadballApp/>,
-        document.getElementById('root')
-)
+ReactDOM.render((
+      <Router history={hashHistory}>
+        <Route path="/" component={DreadballApp}/>
+      </Router>
+    ), document.getElementById('root'))
