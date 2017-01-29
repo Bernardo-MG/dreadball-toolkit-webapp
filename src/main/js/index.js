4b44f48a74196eb78d32b233ae1afa005d0d9e23
Bernardo <programming@wandrell.com>
2017-01-29 19:17:05 +0100

    Using browser history handler.

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -2,7 +2,7 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom')
-import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import Home from './Home';
 import DbxPlayers from './DbxPlayers';
 import MainLayout from './layout/main';
@@ -10,8 +10,8 @@ import MainLayout from './layout/main';
 require('./theme/style.scss');
 
 ReactDOM.render((
-      <Router history={hashHistory}>
-        <Route path="/" component={MainLayout}>
+      <Router history={browserHistory}>
+        <Route path="/dreadball/" component={MainLayout}>
           <IndexRoute component={Home}/>
           <Route path="/players" activeClassName="dbxPlayersLink" component={DbxPlayers}/>
         </Route>
