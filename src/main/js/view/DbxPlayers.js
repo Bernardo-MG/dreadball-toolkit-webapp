7653af22a5234c9243c1241dc67506076a949803
Bernardo <programming@wandrell.com>
2017-01-29 22:43:58 +0100

    Added a table

--- src/main/js/view/DbxPlayers.js
+++ src/main/js/view/DbxPlayers.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
+import { Table } from 'react-toolbox';
+
+const UnitModel = {
+  name: {type: String},
+  role: {type: String},
+  cost: {type: Number}
+};
+
+const units = [
+  {name: 'Unit', role: 'Jack', cost: 10}
+];
+
+class UnitTable extends React.Component {
+  state = { source: units };
+
+  render () {
+    return (
+      <Table
+        model={UnitModel}
+        source={this.state.source}
+        selectable={false}
+        multiSelectable={false}
+      />
+    );
+  }
+}
+
 class DbxPlayers extends React.Component {
    render() {
          return (
-            <h1>DBX players</h1>
+	         <div>
+	            <h1>DBX players</h1>
+	            <UnitTable/>
+	         </div>
          );
      };
 };
