3d675186919f15d2009c57c88a052430377a37e3
Bernardo <programming@wandrell.com>
2017-02-17 17:36:23 +0100

    Main layout is now a function

--- src/main/js/components/MainLayout.js
+++ src/main/js/components/MainLayout.js
@@ -4,14 +4,12 @@ import MainDrawerContent from './MainDrawerContent';
 
 import GithubIcon from '../icons';
 
-class MainLayout extends React.Component {
-   render() {
-         return (
-             <BaseLayout drawerContent={ <MainDrawerContent/> } rightIcon={ <GithubIcon/> }>
-                {this.props.children}
-             </BaseLayout>
-         );
-     };
+const MainLayout = (props) => {
+     return (
+         <BaseLayout drawerContent={ <MainDrawerContent/> } rightIcon={ <GithubIcon/> }>
+            {props.children}
+         </BaseLayout>
+     );
 };
 
 export default MainLayout;
