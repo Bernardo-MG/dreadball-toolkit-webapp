b438be60fa3653b7d9eeb47243c0d0511a34f271
Bernardo <programming@wandrell.com>
2017-02-25 23:08:46 +0100

    Moved i18n messages to their own file

--- src/main/js/components/DbxUnitTable.js
+++ src/main/js/components/DbxUnitTable.js
@@ -1,53 +1,7 @@
 import React from 'react';
 import { Table } from 'react-toolbox';
 import { defineMessages, injectIntl } from 'react-intl';
-
-const unitMessages = defineMessages({
-  name: {
-    id: 'unit.name',
-    defaultMessage: 'Name',
-  },
-  role: {
-    id: 'unit.role',
-    defaultMessage: 'Role',
-  },
-  move: {
-    id: 'unit.move',
-    defaultMessage: 'Move',
-  },
-  strength: {
-    id: 'unit.strength',
-    defaultMessage: 'Strength',
-  },
-  speed: {
-    id: 'unit.speed',
-    defaultMessage: 'Speed',
-  },
-  skill: {
-    id: 'unit.skill',
-    defaultMessage: 'Skill',
-  },
-  armor: {
-    id: 'unit.armor',
-    defaultMessage: 'Armor',
-  },
-  abilities: {
-    id: 'unit.abilities',
-    defaultMessage: 'Abilities',
-  },
-  stranger_cost: {
-    id: 'unit.cost.stranger',
-    defaultMessage: 'Stranger cost',
-  },
-  ally_cost: {
-    id: 'unit.cost.ally',
-    defaultMessage: 'Ally cost',
-  },
-  friend_cost: {
-    id: 'unit.cost.friend',
-    defaultMessage: 'Friend cost',
-  }
-});
+import unitMessages from '../i18n/unit';
 
 class DbxUnitTable extends React.Component {
     UnitModel = {}
