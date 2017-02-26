9c576496bd7b30313869c8aa46ac8480750f0586
Bernardo <programming@wandrell.com>
2017-02-26 13:47:04 +0100

    Added ability i18n messages

---
+++ src/main/js/i18n/ability.js
@@ -0,0 +1,318 @@
+import { defineMessages } from 'react-intl';
+
+const unitMessages = defineMessages({
+   360_vision: {
+      id: 'ability.360vision',
+      defaultMessage: '360 vision'
+   },
+   adaptable: {
+      id: 'ability.adaptable',
+      defaultMessage: 'Adaptable'
+   },
+   a_safe_pair_of_hands: {
+      id: 'ability.aSafePairOfHands',
+      defaultMessage: 'A safe pair of hands'
+   },
+   alert: {
+      id: 'ability.alert',
+      defaultMessage: 'Alert'
+   },
+   backflip: {
+      id: 'ability.backflip',
+      defaultMessage: 'Backflip'
+   },
+   blood_money: 
+      id: 'ability.bloodMoney',
+      defaultMessage: 'Blood money'
+   },
+   cant_feel_a_thing: {
+      id: 'ability.cantFeelThing',
+      defaultMessage: 'Can\'t feel a thing'
+   },
+   charge: {
+      id: 'ability.charge',
+      defaultMessage: 'Charge'
+   },
+   comin_through: {
+      id: 'ability.cominThrough',
+      defaultMessage: 'Comin\' through'
+   },
+   crowd_puller: {
+      id: 'ability.crowd_puller',
+      defaultMessage: 'Crowd puller'
+   },
+   dirty_tricks: {
+      id: 'ability.dirtyTricks',
+      defaultMessage: 'Dirty tricks'
+   },
+   does_this_hurt: {
+      id: 'ability.doesThisHurt',
+      defaultMessage: 'Does this hurt?'
+   },
+   driller: {
+      id: 'ability.driller',
+      defaultMessage: 'Driller'
+   },
+   duck_and_weave: {
+      id: 'ability.duckAndWeave',
+      defaultMessage: 'Duck and weave'
+   },
+   even_the_odds: {
+      id: 'ability.evenTheOdds',
+      defaultMessage: 'Even the odds'
+   },
+   explosive_collar: {
+      id: 'ability.explosiveCollar',
+      defaultMessage: 'Explosive collar'
+   },
+   fan_favourite: {
+      id: 'ability.fanFavourite',
+      defaultMessage: 'Fan favourite'
+   },
+   fragile: {
+      id: 'ability.fragile',
+      defaultMessage: 'Fragile'
+   },
+   gotcha: {
+      id: 'ability.gotcha',
+      defaultMessage: 'Gotcha'
+   },
+   grizzled: {
+      id: 'ability.grizzled',
+      defaultMessage: 'Grizzled'
+   },
+   harmonics: {
+      id: 'ability.harmonics',
+      defaultMessage: 'Harmonics'
+   },
+   heal: {
+      id: 'ability.heal',
+      defaultMessage: 'Heal'
+   },
+   illegal: {
+      id: 'ability.illegal',
+      defaultMessage: 'Illegal'
+   },
+   illegal_modifications: {
+      id: 'ability.illegalModifications',
+      defaultMessage: 'Illegal modifications'
+   },
+   it_wasnt_me: {
+      id: 'ability.itWasntMe',
+      defaultMessage: 'Harmonics'
+   },
+   jump: {
+      id: 'ability.jump',
+      defaultMessage: 'Jump'
+   },
+   keeper: {
+      id: 'ability.keeper',
+      defaultMessage: 'Keeper'
+   },
+   klutz: {
+      id: 'ability.klutz',
+      defaultMessage: 'Klutz'
+   },
+   long_arms: {
+      id: 'ability.longArms',
+      defaultMessage: 'Long arms'
+   },
+   lucky: {
+      id: 'ability.lucky',
+      defaultMessage: 'Lucky'
+   },
+   mighty: {
+      id: 'ability.mighty',
+      defaultMessage: 'Mighty'
+   },
+   mind_control: {
+      id: 'ability.mindControl',
+      defaultMessage: 'Mind control'
+   },
+   mind_like_water: {
+      id: 'ability.mindLikeWater',
+      defaultMessage: 'Mind like water'
+   },
+   misdirect: {
+      id: 'ability.misdirect',
+      defaultMessage: 'Misdirect'
+   },
+   nemesis: {
+      id: 'ability.nemesis',
+      defaultMessage: 'Nemesis'
+   },
+   one_mind: {
+      id: 'ability.oneMind',
+      defaultMessage: 'One mind'
+   },
+   pacifist: {
+      id: 'ability.pacifist',
+      defaultMessage: 'Pacifist'
+   },
+   phaser: {
+      id: 'ability.phaser',
+      defaultMessage: 'Phaser'
+   },
+   pile_driver: {
+      id: 'ability.pileDriver',
+      defaultMessage: 'Pile driver'
+   },
+   poison_blade: {
+      id: 'ability.poisonBlade',
+      defaultMessage: 'Poison blade'
+   },
+   portal: {
+      id: 'ability.portal',
+      defaultMessage: 'Portal'
+   },
+   portal_master: {
+      id: 'ability.portalMaster',
+      defaultMessage: 'Portal master'
+   },
+   prima_donna: {
+      id: 'ability.primaDonna',
+      defaultMessage: 'Prima donna'
+   },
+   pummel: {
+      id: 'ability.pummel',
+      defaultMessage: 'Pummel'
+   },
+   push: {
+      id: 'ability.push',
+      defaultMessage: 'Push'
+   },
+   quick_change_artist: {
+      id: 'ability.quickChangeArtist',
+      defaultMessage: 'Quick change artist'
+   },
+   quick_recovery: {
+      id: 'ability.quickRecovery',
+      defaultMessage: 'Quick recovery'
+   },
+   ram: {
+      id: 'ability.ram',
+      defaultMessage: 'Ram'
+   },
+   ray_gun: {
+      id: 'ability.rayGun',
+      defaultMessage: 'Ray gun'
+   },
+   really_lucky: {
+      id: 'ability.reallyLucky',
+      defaultMessage: 'Really lucky'
+   },
+   resilient: {
+      id: 'ability.resilient',
+      defaultMessage: 'Resilient'
+   },
+   resources: {
+      id: 'ability.resources',
+      defaultMessage: 'Resources'
+   },
+   roll: {
+      id: 'ability.roll',
+      defaultMessage: 'Roll'
+   },
+   rolling: {
+      id: 'ability.rolling',
+      defaultMessage: 'Rolling'
+   },
+   rule_of_law: {
+      id: 'ability.ruleOfLaw',
+      defaultMessage: 'Rule of law'
+   },
+   runaround: {
+      id: 'ability.runaround',
+      defaultMessage: 'Runaround'
+   },
+   running_interference: {
+      id: 'ability.runningInterference',
+      defaultMessage: 'Running interference'
+   },
+   shock_collar: {
+      id: 'ability.shockCollar',
+      defaultMessage: 'Shock collar'
+   },
+   shove: {
+      id: 'ability.shove',
+      defaultMessage: 'Shove'
+   },
+   show_off: {
+      id: 'ability.showOff',
+      defaultMessage: 'Show off'
+   },
+   slide: {
+      id: 'ability.slide',
+      defaultMessage: 'Slide'
+   },
+   slippery_customer: {
+      id: 'ability.slipperyCustomer',
+      defaultMessage: 'Slippery customer'
+   },
+   snack: {
+      id: 'ability.snack',
+      defaultMessage: 'Snack'
+   },
+   spinner: {
+      id: 'ability.spinner',
+      defaultMessage: 'Spinner'
+   },
+   steady: {
+      id: 'ability.steady',
+      defaultMessage: 'Steady'
+   },
+   stench: {
+      id: 'ability.stench',
+      defaultMessage: 'Stench'
+   },
+   stretch: {
+      id: 'ability.stretch',
+      defaultMessage: 'Stretch'
+   },
+   stubborn: {
+      id: 'ability.stubborn',
+      defaultMessage: 'Stubborn'
+   },
+   tail: {
+      id: 'ability.tail',
+      defaultMessage: 'Tail'
+   },
+   taking_a_dive: {
+      id: 'ability.takingDive',
+      defaultMessage: 'Taking a dive'
+   },
+   teleport: {
+      id: 'ability.teleport',
+      defaultMessage: 'Teleport'
+   },
+   threatening: {
+      id: 'ability.threatening',
+      defaultMessage: 'Threatening'
+   },
+   tongue: {
+      id: 'ability.tongue',
+      defaultMessage: 'Tongue'
+   },
+   toxic: {
+      id: 'ability.toxic',
+      defaultMessage: 'Toxic'
+   },
+   toxic_immunity: {
+      id: 'ability.toxicImmunity',
+      defaultMessage: 'Toxic immunity'
+   },
+   trail_blazer: {
+      id: 'ability.trailBlazer',
+      defaultMessage: 'Trail blazer'
+   },
+   uncontrolled: {
+      id: 'ability.uncontrolled',
+      defaultMessage: 'Uncontrolled'
+   },
+   vigour: {
+      id: 'ability.vigour',
+      defaultMessage: 'Vigour'
+   }
+});
+
+export default unitMessages;
