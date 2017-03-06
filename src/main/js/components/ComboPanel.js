67b9a423884208d2314282696cbb43ab6ad0ea8a
Bernardo <programming@wandrell.com>
2017-03-06 21:46:48 +0100

    The combo panel now can receive the values as parameters

--- src/main/js/components/ComboPanel.js
+++ src/main/js/components/ComboPanel.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import Combobox from './Combobox';
 
-const affinities = [
-   { value: 'affinity1', label: 'affinity1' },
-   { value: 'affinity2', label: 'affinity2'},
-   { value: 'affinity3', label: 'affinity3' },
-   { value: 'affinity4', label: 'affinity4'},
-   { value: 'affinity5', label: 'affinity5'},
-   { value: 'affinity6', label: 'affinity6'}
-];
-
-const values = [ affinities, affinities, affinities, affinities, affinities ];
-
-const ComboPanel = () => {
+const ComboPanel = (props) => {
    return (
       <div>
-         {values.map(function(element, i) {
+         {props.source.map(function(element, i) {
             return (
-               <Combobox source={element} />
+               <Combobox key={i} source={element} />
             );
          })}
       </div>
--- src/main/js/components/views/builder/SponsorAffinities.js
+++ src/main/js/components/views/builder/SponsorAffinities.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ComboPanel from '../../../components/ComboPanel';
 
+const affinities = [
+   { value: 'affinity1', label: 'affinity1' },
+   { value: 'affinity2', label: 'affinity2' },
+   { value: 'affinity3', label: 'affinity3' },
+   { value: 'affinity4', label: 'affinity4' },
+   { value: 'affinity5', label: 'affinity5' },
+   { value: 'affinity6', label: 'affinity6' }
+];
+
+const values = [ affinities, affinities, affinities, affinities, affinities ];
+
 const SponsorAffinities = () => {
    return (
-      <ComboPanel />
+      <ComboPanel source={values} />
    );
 };
 
