b2a868c39f9a02ef9ce25845003ab14869c6086b
Bernardo <programming@wandrell.com>
2017-03-13 21:15:18 +0100

    The REST URLs now use the base route

--- src/main/js/constants/RestUrls.js
+++ src/main/js/constants/RestUrls.js
@@ -1,3 +1,3 @@
-export const AFFINITY_UNITS_REST_ENDPOINT = './rest/units'
+export const AFFINITY_UNITS_REST_ENDPOINT = ROUTE_BASE + '/rest/units'
 
-export const AFFINITY_GROUPS_REST_ENDPOINT = './rest/affinityGroups'
+export const AFFINITY_GROUPS_REST_ENDPOINT = ROUTE_BASE + '/rest/affinityGroups'
