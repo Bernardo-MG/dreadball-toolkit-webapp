3963dcba736192633d94160c632d646e50f11275
Bernardo <programming@wandrell.com>
2017-03-14 10:33:08 +0100

    The rank increase option is handled

---
+++ src/main/js/i18n/teamBuilder.js
@@ -0,0 +1,10 @@
+import { defineMessages } from 'react-intl';
+
+const unitNameMessages = defineMessages({
+   increase_rank_option: {
+      id: 'builder.text.increaseRankOption',
+      defaultMessage: 'Rank +1'
+   }
+});
+
+export default unitNameMessages;
--- src/main/js/utils/dbxBuilder.js
+++ src/main/js/utils/dbxBuilder.js
@@ -1,29 +1,33 @@
 import abilityMessages from '../i18n/ability';
 import roleMessages from '../i18n/role';
 import affinityMessages from '../i18n/affinity';
+import builderMessages from '../i18n/teamBuilder';
 
 export const transformSponsorAffinityGroupAvailabilitiesJson = (json, intl) => {
    var result = [];
    var entry;
+   var affinities;
    var mapped;
    
    for (var i=0; i<json.length; i++) {
       entry = json[i];
-      mapped = transformAffinityGroupsJson(entry.affinityGroups, intl);
+      affinities = transformAffinityGroupsJson(entry.affinityGroups, intl);
+   
+      addRankIncrease(entry.includingRankIncrease, affinities, intl);
       
-      result.push(mapped);
+      result.push(affinities);
    };
    
    return result;
 }
 
-const transformAffinityGroupsJson = (json, intl) => {
+const transformAffinityGroupsJson = (affinityGroups, intl) => {
    var result = [];
    var entry;
    var mapped;
    
-   for (var i=0; i<json.length; i++) {
-      entry = json[i];
+   for (var i=0; i<affinityGroups.length; i++) {
+      entry = affinityGroups[i];
       mapped = {
          label : intl.formatMessage(affinityMessages[entry.name]),
          value : entry.name
@@ -33,4 +37,17 @@ const transformAffinityGroupsJson = (json, intl) => {
    };
    
    return result;
+}
+
+const addRankIncrease = (rank, affinities, intl) => {
+   var mapped;
+   
+   if(rank){
+      mapped = {
+         label : intl.formatMessage(builderMessages['increase_rank_option']),
+         value : 'rank'
+      }
+      
+      affinities.push(mapped);
+   }
 }
