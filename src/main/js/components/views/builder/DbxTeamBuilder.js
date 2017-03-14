7da3bbb20a5b69dac54d6f0a3820429f1bf43739
Bernardo <programming@wandrell.com>
2017-03-14 23:08:24 +0100

    Simplified view

--- src/main/js/components/views/builder/DbxTeamBuilder.js
+++ src/main/js/components/views/builder/DbxTeamBuilder.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react'
 import { Button } from 'react-toolbox/lib/button';
 import AffinityGroupsComboPanel from '../../../containers/AffinityGroupsComboPanel';
 
+const SponsorAffinitiesView = (props) => {
+   return (
+      <div>
+         <AffinityGroupsComboPanel />
+         <Button onClick={ props.onClick } label='accept' raised primary />
+      </div>
+   );
+};
+
+const SponsorTeamView = () => {
+   return (
+      <div />
+   );
+};
 
 class DbxTeamBuilder extends Component {
 
@@ -17,18 +31,18 @@ class DbxTeamBuilder extends Component {
 
    render() {
       let view = null;
+      
       if(!this.state.affinitiesChosen) { 
-         return (
-            <div>
-               <AffinityGroupsComboPanel />
-               <Button onClick={ this.choosedAffinities } label='accept' raised primary />
-            </div>
-         );
+         view = <SponsorAffinitiesView onClick={ this.choosedAffinities } />;
       } else {
-         return (
-            <div/>
-         );
+         view = <SponsorTeamView />;
       }
+      
+      return (
+         <div>
+            { view }
+         </div>
+      );
    };
    
 };
