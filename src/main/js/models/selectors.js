98952ff94e6fb52bae15ab21c737f7a4eb1ed34d
Bernardo <programming@wandrell.com>
2017-04-14 00:10:44 +0200

    Players are selected along their abilities

--- src/main/js/models/selectors.js
+++ src/main/js/models/selectors.js
@@ -1,23 +1,36 @@
-import { createSelector } from 'redux-orm';
+import { createSelector } from 'reselect';
+import { createSelector as ormCreateSelector } from 'redux-orm';
 import orm from 'models';
 
 // Selects the state managed by Redux-ORM.
 export const ormSelector = state => state.orm;
 
 export const players = createSelector(
-   orm,
    ormSelector,
-   orm => {
-      //return orm.Player.withRefs.map(player => {
-         //const obj = Object.assign({}, player.ref);
-         
-         //obj.abilities = player.abilities.withRefs.map(ability => ability.name);
-         
-         //return obj;
-         //return player;
-      //});
-      //return orm.Player.all().toRefArray();
-      const result = orm.Player.all().toRefArray();
-      return orm.Player.all().toRefArray();
-   }
+   state => state,
+   ormCreateSelector(
+      orm,
+      session => {
+         //return orm.Player.withRefs.map(player => {
+            //const obj = Object.assign({}, player.ref);
+            
+            //obj.abilities = player.abilities.withRefs.map(ability => ability.name);
+            
+            //return obj;
+            //return player;
+         //});
+         //return orm.Player.all().toRefArray();
+         //const result = orm.Player.all().toRefArray();
+         var result = session.Player.all().toModelArray();
+         result = result.map(player => {
+            const obj = Object.assign({}, player.ref);
+            if(player.abilities) {
+               obj.abilities = player.abilities.toRefArray().map(ability => ability.name);
+            }
+            return obj;
+         });
+         return result;
+         //return session.Player.all().toRefArray();
+      }
+   )
 );
