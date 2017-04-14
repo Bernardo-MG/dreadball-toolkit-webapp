e6c4be52c5964d79989a020c182439e87c6da48e
Bernardo <programming@wandrell.com>
2017-04-14 14:01:13 +0200

    Removed unused code

--- src/main/js/models/selectors.js
+++ src/main/js/models/selectors.js
@@ -11,26 +11,19 @@ export const players = createSelector(
    ormCreateSelector(
       orm,
       session => {
-         //return orm.Player.withRefs.map(player => {
-            //const obj = Object.assign({}, player.ref);
-            
-            //obj.abilities = player.abilities.withRefs.map(ability => ability.name);
-            
-            //return obj;
-            //return player;
-         //});
-         //return orm.Player.all().toRefArray();
-         //const result = orm.Player.all().toRefArray();
          var result = session.Player.all().toModelArray();
+         
          result = result.map(player => {
             const obj = Object.assign({}, player.ref);
+            
             if(player.abilities) {
                obj.abilities = player.abilities.toRefArray().map(ability => ability.name);
             }
+            
             return obj;
          });
+         
          return result;
-         //return session.Player.all().toRefArray();
       }
    )
 );
