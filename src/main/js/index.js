d0430be278aa0212cbccebe78e8defebfe2b171c
Bernardo <programming@wandrell.com>
2017-04-15 21:19:20 +0200

    Set absolute imports

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -3,11 +3,11 @@
 const React = require('react');
 import { render } from 'react-dom'
 
-import Root from './containers/Root'
+import Root from 'containers/Root'
 import { createHistory } from 'history'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { useRouterHistory } from 'react-router'
-import configureStore from './store/configureStore'
+import configureStore from 'store/configureStore'
 
 require('./theme/style.scss');
 
