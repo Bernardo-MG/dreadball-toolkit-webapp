1840fc318647146ac2e6d972b8b6edf7d3264029
Bernardo <programming@wandrell.com>
2017-04-16 21:01:43 +0200

    Removed unused code

--- src/main/js/actions/request/unit.js
+++ src/main/js/actions/request/unit.js
@@ -1,29 +1,15 @@
 import { REQUEST_UNITS, REQUEST_UNITS_SUCCESS, REQUEST_UNITS_FAILURE } from 'constants/ActionTypes'
 import { AFFINITY_UNITS_REST_ENDPOINT as URL } from 'constants/RestUrls'
 import { transformAffinityUnitsJson as parse } from 'utils/codex'
-import { fetchData } from 'utils/request'
 import { CALL_API } from 'middleware/api'
 
-export const request = () => ({
-   type: REQUEST_UNITS
-})
-
-export const requestSuccess = (data) => ({
-   type: REQUEST_UNITS_SUCCESS,
-   payload: data
-})
-
-const fetchUnits = () => ({
+export const fetch = (affinities) => ({
   [CALL_API]: {
     types: [ REQUEST_UNITS, REQUEST_UNITS_SUCCESS, REQUEST_UNITS_FAILURE ],
     endpoint: URL,
     parse: parse
   }
 })
-
-export const fetch = (affinities) => dispatch => {
-   dispatch(fetchUnits())
-}
 
 const parseAffinitiesUrl = (url, affinities) => {
    var result;
@@ -34,10 +20,4 @@ const parseAffinitiesUrl = (url, affinities) => {
    }
    
    return result;
-}
-
-const handleReceive = (json, dispatch) => {
-   const units = parse(json);
-   
-   dispatch(requestSuccess(units))
 }
--- src/main/js/utils/request.js
+++
@@ -1,6 +0,0 @@
-import fetch from 'isomorphic-fetch'
-
-export const fetchData = (url, parse) =>
-   fetch(url)
-      .then(response => response.json())
-      .then(json => parse(json))
