52ec70e94340943b3ee9b4fe5af1dd7eeea139f0
Bernardo <programming@wandrell.com>
2017-05-05 16:47:34 +0200

    Index is used for exporting reducer

--- src/main/js/reducers/index.js
+++ src/main/js/reducers/index.js
@@ -3,7 +3,7 @@ import { routerReducer as routing } from 'react-router-redux'
 import { createReducer } from 'redux-orm';
 import dbxBuilder from 'reducers/dbxBuilder'
 import orm from 'models';
-import paginate from 'pagination/reducers/pagination'
+import { paginate } from 'pagination/reducers'
 import { REQUEST_UNITS, REQUEST_UNITS_SUCCESS, REQUEST_UNITS_FAILURE } from 'requests/actions/ActionTypes'
 
 const pagination = combineReducers({
