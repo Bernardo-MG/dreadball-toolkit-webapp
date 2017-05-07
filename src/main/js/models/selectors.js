3f7f1de5a61f28dfd4462ef61d06f7760ae10dc5
Bernardo <programming@wandrell.com>
2017-05-07 20:05:42 +0200

    Simplified paginated selector

--- src/main/js/models/selectors.js
+++ src/main/js/models/selectors.js
@@ -1,30 +1,23 @@
 import { createSelector } from 'reselect';
 import { createSelector as ormCreateSelector } from 'redux-orm';
 import orm from 'models';
+import { filterPaginated } from 'pagination/utils';
 
 // Selects the state managed by Redux-ORM.
-export const ormSelector = state => state.orm;
+const ormSelector = state => state.orm;
+
+const loadUnit = (unit) => {
+   const obj = Object.assign({}, unit.ref);
+   
+   if(unit.abilities) {
+      obj.abilities = unit.abilities.toRefArray().map(ability => ability.name);
+   }
+   
+   return obj;
+};
 
 export const units = createSelector(
    ormSelector,
    state => state.pagination.units,
-   ormCreateSelector(orm, (session, pagination) => {
-         var result = session.Player.all().toModelArray();
-         const start = pagination.page * pagination.elements;
-         const end = start + pagination.elements;
-         const ids = pagination.ids.slice(start, end);
-         
-         result = result.filter(unit => ids.includes(unit.templateName)).map(unit => {
-            const obj = Object.assign({}, unit.ref);
-            
-            if(unit.abilities) {
-               obj.abilities = unit.abilities.toRefArray().map(ability => ability.name);
-            }
-            
-            return obj;
-         });
-         
-         return result;
-      }
-   )
+   ormCreateSelector(orm, filterPaginated(loadUnit, (session) => session.Player))
 );
--- src/main/js/pagination/utils.js
+++ src/main/js/pagination/utils.js
@@ -18,3 +18,16 @@ export const previousPage = (fetch, current, first) => {
    
    fetch(page);
 }
+
+export const filterPaginated = (loader, model) => (session, pagination) => {
+   var result = model(session).all().toModelArray();
+   const start = pagination.page * pagination.elements;
+   const end = start + pagination.elements;
+   const ids = pagination.ids.slice(start, end);
+   
+   result = result.filter(entity => ids.includes(entity.templateName)).map(entity => {
+      return loader(entity);
+   });
+   
+   return result;
+};
