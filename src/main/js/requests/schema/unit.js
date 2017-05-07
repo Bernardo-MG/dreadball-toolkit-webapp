94725a6eae0964f82529211bef24c48cbbc6e3d1
Bernardo <programming@wandrell.com>
2017-05-07 16:10:47 +0200

    Moved normalizer schemas to their own file

---
+++ src/main/js/requests/schema/unit.js
@@ -0,0 +1,17 @@
+import { schema } from 'normalizr';
+
+export const ability = new schema.Entity('abilities', {}, {
+   idAttribute: 'name'
+});
+
+export const affinity = new schema.Entity('affinities', {}, {
+   idAttribute: 'name'
+});
+
+export const unit = new schema.Entity('units', {
+   abilities: [ ability ],
+   affinityGroups: [ affinity ],
+   hatedAffinityGroups: [ affinity ]
+}, {
+   idAttribute: 'templateName'
+});
--- src/main/js/requests/utils/unit.js
+++ src/main/js/requests/utils/unit.js
@@ -1,20 +1,5 @@
-import { normalize, schema } from 'normalizr';
-
-const ability = new schema.Entity('abilities', {}, {
-   idAttribute: 'name'
-});
-
-const affinity = new schema.Entity('affinities', {}, {
-   idAttribute: 'name'
-});
-
-const unit = new schema.Entity('units', {
-   abilities: [ ability ],
-   affinityGroups: [ affinity ],
-   hatedAffinityGroups: [ affinity ]
-}, {
-   idAttribute: 'templateName'
-});
+import { normalize } from 'normalizr';
+import { unit } from 'requests/schema';
 
 export const jsonToUnits = (json) => {
    return normalize(json, [unit]);
