c2e19042c73dd1022bf9e8219a622a187dd1ee4e
Bernardo <programming@wandrell.com>
2017-05-15 21:27:38 +0200

    Corrected script

--- src/main/js/requests/utils/sponsorAffAva.js
+++ src/main/js/requests/utils/sponsorAffAva.js
@@ -1,6 +1,6 @@
 import { normalize } from 'normalizr';
 import { sponsorAffinityAvailability } from 'requests/schema';
 
-export const jsonToSponsorAffinityGroupAvailabilities = (json) => ({
+export const jsonToSponsorAffinityGroupAvailabilities = (json) => {
    return normalize(json, [sponsorAffinityAvailability]);
-});
+};
