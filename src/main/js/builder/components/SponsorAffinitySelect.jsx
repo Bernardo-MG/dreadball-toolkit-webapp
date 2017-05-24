effb01ad0c2a3006d397e9cdf57e434ed29e4b9b
Bernardo <programming@wandrell.com>
2017-05-24 22:17:17 +0200

    Moved affinities combo box logic to a component

---
+++ src/main/js/builder/components/SponsorAffinitySelect.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Select from 'grommet/components/Select';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as Actions from 'builder/actions';
+
+class SponsorAffinitySelect extends Component {
+
+   state = {};
+   index;
+   handleSelection;
+
+   handleChange = (value) => {
+      const selected = value.option.value;
+      const affinity = value.option.affinity;
+      const rank = value.option.rank;
+      this.setState({ value : selected, affinity, rank });
+
+      this.handleSelection(affinity, rank, index);
+   };
+
+   componentDidMount() {
+      this.props.handleSelection(this.state.value, this.state.rank, this.index);
+   }
+
+   constructor(props) {
+      super(props);
+      this.index = props.index;
+      this.handleSelection = props.handleSelection;
+      this.state.affinity = props.source[0].affinity;
+      this.state.value = props.source[0].value;
+      this.state.rank = props.source[0].rank;
+   }
+
+   render() {
+      return (
+         <Select placeHolder='None'
+            options={this.props.source}
+            value={this.state.value}
+            onChange={this.handleChange} />
+      );
+   }
+}
+
+export default SponsorAffinitySelect;
--- src/main/js/builder/containers/SponsorAffinityComboBox.jsx
+++ src/main/js/builder/containers/SponsorAffinityComboBox.jsx
@@ -1,40 +1,15 @@
 import React, { Component } from 'react';
 import Select from 'grommet/components/Select';
+import SponsorAffinitySelect from 'builder/components/SponsorAffinitySelect';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as Actions from 'builder/actions';
 
 class SponsorAffinityComboBox extends Component {
 
-   state = {};
-   index;
-
-   handleChange = (value) => {
-      const selected = value.option.value;
-      const affinity = value.option.affinity;
-      const rank = value.option.rank;
-      this.setState({ value : selected, affinity, rank });
-      this.props.actions.chooseSponsorAffinity(affinity, rank, this.index);
-   };
-
-   constructor(props) {
-      super(props);
-      this.index = props.index;
-      this.state.affinity = props.source[0].affinity;
-      this.state.value = props.source[0].value;
-      this.state.rank = props.source[0].rank;
-   }
-
-   componentDidMount() {
-      this.props.actions.chooseSponsorAffinity(this.state.value, this.state.rank, this.index);
-   }
-
    render() {
       return (
-         <Select placeHolder='None'
-            options={this.props.source}
-            value={this.state.value}
-            onChange={this.handleChange} />
+         <SponsorAffinitySelect source={this.props.source} handleSelection={this.props.actions.chooseSponsorAffinity} />
       );
    }
 }
