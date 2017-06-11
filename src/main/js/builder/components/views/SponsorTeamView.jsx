ac07ad929108a43b3ddb9376dd8e25428a7d2037
Bernardo <programming@wandrell.com>
2017-06-11 15:41:59 +0200

    Added number fields

--- src/main/js/builder/components/views/SponsorTeamView.jsx
+++ src/main/js/builder/components/views/SponsorTeamView.jsx
@@ -6,8 +6,10 @@ import SponsorAffinityList from 'builder/containers/SponsorAffinityList';
 import Form from 'grommet/components/Form';
 import FormField from 'grommet/components/FormField';
 import TextInput from 'grommet/components/TextInput';
+import NumberInput from 'grommet/components/NumberInput';
 import SponsorRankValue from 'builder/containers/SponsorRankValue';
 import Value from 'grommet/components/Value';
+import Button from 'grommet/components/Button';
 //import { Button } from 'react-toolbox/lib/button';
 //import { Layout, NavDrawer, Panel, Sidebar } from 'react-toolbox';
 //import { Card, CardActions } from 'react-toolbox/lib/card';
@@ -51,6 +53,26 @@ class SponsorTeamView extends React.Component {
                   </FormField>
                   <SponsorAffinityList />
                   <SponsorRankValue />
+                  <h1>spend_rank</h1>
+                  <Button label='additional_affinity' />
+                  <FormField label='coaching_dice'>
+                     <NumberInput id='coaching_dice' name='coaching_dice' max={100}/>
+                  </FormField>
+                  <FormField label='special_move_card'>
+                     <NumberInput id='special_move_card' name='special_move_card' max={100}/>
+                  </FormField>
+                  <FormField label='nasty_surprise_card'>
+                     <NumberInput id='nasty_surprise_card' name='nasty_surprise_card' max={100}/>
+                  </FormField>
+                  <FormField label='wager'>
+                     <NumberInput id='wager' name='wager' max={100}/>
+                  </FormField>
+                  <FormField label='medibot'>
+                     <NumberInput id='medibot' name='medibot' max={100}/>
+                  </FormField>
+                  <FormField label='cheerleaders'>
+                     <NumberInput id='cheerleaders' name='cheerleaders' max={100}/>
+                  </FormField>
                </Form>
             </div>
 //         <Layout>
