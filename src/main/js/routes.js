2aa0b0364141403dd5b958f1dae82cc88e96a708
Bernardo <programming@wandrell.com>
2017-07-16 20:06:48 +0200

    Removed redundant layout

--- src/main/js/routes.js
+++ src/main/js/routes.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import DbxPlayers from 'codex/components/views/DbxPlayers';
 import ContainerDbxTeamBuilder from 'builder/containers/views/ContainerDbxTeamBuilder';
-import MainLayout from 'components/layout/MainLayout';
+import BaseLayout from 'components/layout/BaseLayout';
 import Home from 'components/views/Home';
 
-export default <Route path="/" component={MainLayout}>
+export default <Route path="/" component={BaseLayout}>
    <IndexRoute component={Home}/>
    <Route path="/players" activeClassName="dbxPlayersLink" component={DbxPlayers}/>
    <Route path="/dbx" activeClassName="dbxTeamBuilderLink" component={ContainerDbxTeamBuilder}/>
