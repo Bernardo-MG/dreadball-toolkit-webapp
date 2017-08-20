00e0d3fa69afff25e305f86657433a7015c41e4e
Bernardo <programming@wandrell.com>
2017-08-20 22:10:59 +0200

    Assets fields are now set into two columns

--- src/main/js/builder/components/forms/SponsorAssetsForm.jsx
+++ src/main/js/builder/components/forms/SponsorAssetsForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Box from 'grommet/components/Box';
 import Form from 'grommet/components/Form';
 import FormField from 'grommet/components/FormField';
 
@@ -12,24 +13,30 @@ import WagerInput from 'builder/containers/input/WagerInput';
 
 const SponsorAssetsForm = () =>
    <Form>
-      <FormField label='coaching_dice'>
-         <CoachingDiceInput id='coaching_dice' name='coaching_dice' min={0} max={100}/>
-      </FormField>
-      <FormField label='special_move_card'>
-         <SpecialMoveCardInput id='special_move_card' name='special_move_card' min={0} max={100}/>
-      </FormField>
-      <FormField label='nasty_surprise_card'>
-         <NastySurpriseCardInput id='nasty_surprise_card' name='nasty_surprise_card' min={0} max={100}/>
-      </FormField>
-      <FormField label='wager'>
-         <WagerInput id='wager' name='wager' min={0} max={100}/>
-      </FormField>
-      <FormField label='medibot'>
-         <MediBotInput id='medibot' name='medibot' min={0} max={100}/>
-      </FormField>
-      <FormField label='cheerleaders'>
-         <CheerleadersInput id='cheerleaders' name='cheerleaders' min={0} max={100}/>
-      </FormField>
+      <Box direction='row'>
+         <FormField label='coaching_dice'>
+            <CoachingDiceInput id='coaching_dice' name='coaching_dice' min={0} max={100}/>
+         </FormField>
+         <FormField label='special_move_card'>
+            <SpecialMoveCardInput id='special_move_card' name='special_move_card' min={0} max={100}/>
+         </FormField>
+      </Box>
+      <Box direction='row'>
+         <FormField label='nasty_surprise_card'>
+            <NastySurpriseCardInput id='nasty_surprise_card' name='nasty_surprise_card' min={0} max={100}/>
+         </FormField>
+         <FormField label='wager'>
+            <WagerInput id='wager' name='wager' min={0} max={100}/>
+         </FormField>
+      </Box>
+      <Box direction='row'>
+         <FormField label='medibot'>
+            <MediBotInput id='medibot' name='medibot' min={0} max={100}/>
+         </FormField>
+         <FormField label='cheerleaders'>
+            <CheerleadersInput id='cheerleaders' name='cheerleaders' min={0} max={100}/>
+         </FormField>
+      </Box>
    </Form>;
 
 export default SponsorAssetsForm;
