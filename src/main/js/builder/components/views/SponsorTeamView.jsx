603abec229cd441861d794792b70e5b3eeafd9ba
Bernardo <programming@wandrell.com>
2017-09-07 00:22:41 +0200

    Removed unused components

--- src/main/js/builder/components/views/SponsorTeamView.jsx
+++ src/main/js/builder/components/views/SponsorTeamView.jsx
@@ -5,7 +5,6 @@ import Heading from 'grommet/components/Heading';
 import Tab from 'grommet/components/Tab';
 import Tabs from 'grommet/components/Tabs';
 
-import SponsorAddUnitTable from 'builder/containers/SponsorAddUnitTable';
 import SponsorAffinityList from 'builder/containers/SponsorAffinityList';
 import SponsorUnitList from 'builder/containers/SponsorUnitList';
 
@@ -13,8 +12,6 @@ import SponsorTeamCost from 'builder/components/SponsorTeamCost';
 
 import SponsorAssetsForm from 'builder/components/forms/SponsorAssetsForm';
 import SponsorNameInput from 'builder/containers/SponsorNameInput';
-
-import SponsorTeamEditionLayer from 'builder/components/layers/SponsorTeamEditionLayer';
 
 import BoundDbxTeamPlayersPanel from 'builder/containers/BoundDbxTeamPlayersPanel';
 
@@ -44,57 +41,45 @@ class SponsorTeamView extends Component {
    };
 
    render() {
-      let view = null;
-
-      if (this.state.editingPlayers) {
-         view =
-            <SponsorTeamEditionLayer title='players' onClose={ this.finishEditPlayers }>
-               <SponsorAddUnitTable />
-            </SponsorTeamEditionLayer>;
-      } else {
-         view =
-            <Tabs>
-               <Tab title='First Title'>
-                  <Box>
-                     <Box>
-                        <SponsorTeamCost />
-                     </Box>
-                     <Box>
-                        <SponsorNameInput />
-                     </Box>
-                     <Heading tag='h2'>assets</Heading>
-                     <SponsorAssetsForm />
-                     <Box direction='row'>
-                        <Box size='medium'>
-                           <Box direction='row'>
-                              <Heading tag='h2'>affinities</Heading>
-                              <EditionButton onClick={ this.editAffinities } a11yTitle='edit_affinities' />
-                           </Box>
-                           <SponsorAffinityList />
-                        </Box>
-                        <Box size='medium'>
-                           <Box direction='row'>
-                              <Heading tag='h2'>players</Heading>
-                              <EditionButton onClick={ this.editPlayers } a11yTitle='edit_players' />
-                           </Box>
-                           <SponsorUnitList />
-                        </Box>
-                     </Box>
-                  </Box>
-               </Tab>
-               <Tab title='players'>
+      return (
+         <Tabs>
+            <Tab title='First Title'>
+               <Box>
                   <Box>
                      <SponsorTeamCost />
                   </Box>
                   <Box>
-                     <BoundDbxTeamPlayersPanel />
+                     <SponsorNameInput />
                   </Box>
-               </Tab>
-            </Tabs>;
-      }
-
-      return (
-         view
+                  <Heading tag='h2'>assets</Heading>
+                  <SponsorAssetsForm />
+                  <Box direction='row'>
+                     <Box size='medium'>
+                        <Box direction='row'>
+                           <Heading tag='h2'>affinities</Heading>
+                           <EditionButton onClick={ this.editAffinities } a11yTitle='edit_affinities' />
+                        </Box>
+                        <SponsorAffinityList />
+                     </Box>
+                     <Box size='medium'>
+                        <Box direction='row'>
+                           <Heading tag='h2'>players</Heading>
+                           <EditionButton onClick={ this.editPlayers } a11yTitle='edit_players' />
+                        </Box>
+                        <SponsorUnitList />
+                     </Box>
+                  </Box>
+               </Box>
+            </Tab>
+            <Tab title='players'>
+               <Box>
+                  <SponsorTeamCost />
+               </Box>
+               <Box>
+                  <BoundDbxTeamPlayersPanel />
+               </Box>
+            </Tab>
+         </Tabs>
       );
    }
 
