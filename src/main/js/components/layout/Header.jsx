133cf5365f8ba79d8ac530101ebd2c838bb82384
Bernardo <programming@wandrell.com>
2017-09-08 18:31:35 +0200

    Internationalized menu options

--- src/main/js/components/layout/Header.jsx
+++ src/main/js/components/layout/Header.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import { injectIntl } from 'react-intl';
+
 import Box from 'grommet/components/Box';
 import Header from 'grommet/components/Header';
 import Menu from 'grommet/components/Menu';
+
 import NavAnchor from 'components/NavAnchor';
 import SocialGithubIcon from 'grommet/components/icons/base/SocialGithub';
 
-const AppHeader = () =>
+import titleMessages from 'i18n/title';
+
+const AppHeader = (props) =>
    <Header justify='center' colorIndex='neutral-4' siz='small'>
       <Box size={ { width: { max: 'xxlarge' } } } direction='row'
         responsive={false} justify='start' align='center'
         pad={ { horizontal: 'medium' } } flex='grow'>
         <Menu label='Label' inline={true} direction='row' flex='grow'>
-           <NavAnchor path='/dbx'>dbx_team_creation</NavAnchor>
-           <NavAnchor path='/players'>units_list</NavAnchor>
+           <NavAnchor path='/dbx'>{props.intl.formatMessage(titleMessages.dbxTeamBuilder)}</NavAnchor>
+           <NavAnchor path='/players'>{props.intl.formatMessage(titleMessages.dbxPlayers)}</NavAnchor>
         </Menu>
       <a href={REPO_URL}><SocialGithubIcon /></a>
       </Box>
    </Header>;
 
-export default AppHeader;
+AppHeader.propTypes = {
+   intl: PropTypes.object.isRequired
+};
+
+export default injectIntl(AppHeader);
--- src/main/js/i18n/title.js
+++ src/main/js/i18n/title.js
@@ -2,8 +2,12 @@ import { defineMessages } from 'react-intl';
 
 const unitMessages = defineMessages({
    dbxPlayers: {
-      id: 'title.dbx.players',
+      id: 'title.header.dbx.list',
       defaultMessage: 'DBX Players'
+   },
+   dbxTeamBuilder: {
+      id: 'title.header.dbx.build',
+      defaultMessage: 'DBX Team Builder'
    }
 });
 
