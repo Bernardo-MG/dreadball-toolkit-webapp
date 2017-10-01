34ae3b00e7e8f519323f8d081a824a8238ad7ffd
Bernardo <programming@wandrell.com>
2017-10-01 19:40:55 +0200

    Not using pagination for request which doesn't need it.

--- src/main/js/models/selectors/modelFilters.js
+++ src/main/js/models/selectors/modelFilters.js
@@ -5,10 +5,12 @@ const templateNameSelector = (entity) => entity.templateName;
 
 const nameSelector = (entity) => entity.name;
 
+export const filterAll = (idSelector, loader, modelSelector) => (session) => modelSelector(session).all().toModelArray().map((entity) => loader(entity));
+
 export const playerFilterPaginated = filterByIdPaginated(templateNameSelector, loadPlayer, (session) => session.Player);
 
 export const ratedPlayerFilterPaginated = filterByIdPaginated(templateNameSelector, loadPlayer, (session) => session.RatedPlayer);
 
-export const sponsorAffinityAvailabilityFilterPaginated = filterByIdPaginated(nameSelector, loadSponsorAffAva, (session) => session.SponsorAffinityAvailability);
+export const sponsorAffinityAvailabilityFilterPaginated = filterAll(nameSelector, loadSponsorAffAva, (session) => session.SponsorAffinityAvailability);
 
 export const sponsorPlayerFilter = filterById(templateNameSelector, loadPlayer, (session) => session.RatedPlayer, (payload) => payload.map((unit) => unit.templateName));
--- src/main/js/reducers/index.js
+++ src/main/js/reducers/index.js
@@ -3,8 +3,7 @@ import { routerReducer as routing } from 'react-router-redux';
 import builder from 'builder/reducers';
 import orm from 'models/reducers';
 import paginate from 'pagination/reducers';
-import fetch from 'fetch/reducers';
-import { REQUEST_UNITS, REQUEST_UNITS_SUCCESS, REQUEST_UNITS_FAILURE, REQUEST_SPONSOR_UNITS, REQUEST_SPONSOR_UNITS_SUCCESS, REQUEST_SPONSOR_UNITS_FAILURE, REQUEST_SPONSOR_AFFINITY_GROUP_AVAILABILITIES, REQUEST_SPONSOR_AFFINITY_GROUP_AVAILABILITIES_SUCCESS, REQUEST_SPONSOR_AFFINITY_GROUP_AVAILABILITIES_FAILURE } from 'requests/actions/ActionTypes';
+import { REQUEST_UNITS, REQUEST_UNITS_SUCCESS, REQUEST_UNITS_FAILURE, REQUEST_SPONSOR_UNITS, REQUEST_SPONSOR_UNITS_SUCCESS, REQUEST_SPONSOR_UNITS_FAILURE } from 'requests/actions/ActionTypes';
 
 const pagination = combineReducers({
    units: paginate({
@@ -21,14 +20,6 @@ const pagination = combineReducers({
          REQUEST_SPONSOR_UNITS,
          REQUEST_SPONSOR_UNITS_SUCCESS,
          REQUEST_SPONSOR_UNITS_FAILURE
-      ]
-   }),
-   sponsorAffAvas: fetch({
-      idsMapping: (payload) => Object.keys(payload.entities.sponsorAffinityAvailabilities),
-      types: [
-         REQUEST_SPONSOR_AFFINITY_GROUP_AVAILABILITIES,
-         REQUEST_SPONSOR_AFFINITY_GROUP_AVAILABILITIES_SUCCESS,
-         REQUEST_SPONSOR_AFFINITY_GROUP_AVAILABILITIES_FAILURE
       ]
    })
 });
