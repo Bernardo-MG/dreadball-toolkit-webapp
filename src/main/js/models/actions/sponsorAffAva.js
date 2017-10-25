ba6ec0d39b285879fe21e39f3e1f10179de257c0
Bernardo <programming@wandrell.com>
2017-10-25 22:38:10 +0200

    Fixed builder units no being loaded

--- src/main/js/models/actions/sponsorAffAva.js
+++ src/main/js/models/actions/sponsorAffAva.js
@@ -1,19 +1,17 @@
 import { CREATE_AFFINITIES, CREATE_SPONSOR_AFFINITY_GROUP_AVAILABILITIES } from 'models/actions/ActionTypes';
-import { CALL_API } from 'api/ActionTypes';
 import { SPONSOR_AFFINITY_GROUP_AVAS_REST_ENDPOINT as endpoint } from 'models/Endpoints';
 import { normalize } from 'normalizr';
 import { sponsorAffinityAvailability } from 'models/schema';
 import { fetchPaginated } from 'api/fetch';
+import { callApi } from 'api/actions';
 
 export const parse = (json) => normalize(json, [sponsorAffinityAvailability]);
 
-export const fetch = () => {
-   return {
-      [CALL_API]: {
-         tag: 'SPONSOR_AFFINITY_GROUP_AVAILABILITIES',
-         chained: [CREATE_AFFINITIES, CREATE_SPONSOR_AFFINITY_GROUP_AVAILABILITIES],
-         endpoint,
-         fetch: (url, params) => fetchPaginated(url, params, parse)
-      }
-   };
-};
+const tag = 'SPONSOR_AFFINITY_GROUP_AVAILABILITIES';
+
+export const fetch = () => callApi({
+   tag,
+   chained: [CREATE_AFFINITIES, CREATE_SPONSOR_AFFINITY_GROUP_AVAILABILITIES],
+   endpoint,
+   fetch: (url, params) => fetchPaginated(url, params, parse)
+});
--- src/main/js/models/actions/sponsorUnit.js
+++ src/main/js/models/actions/sponsorUnit.js
@@ -1,23 +1,33 @@
 import { CREATE_ABILITIES, CREATE_AFFINITIES, CREATE_RATED_UNITS } from 'models/actions/ActionTypes';
-import { CALL_API } from 'api/ActionTypes';
 import { SPONSOR_AFFINITY_UNITS_REST_ENDPOINT as endpoint } from 'models/Endpoints';
 import { normalize } from 'normalizr';
 import { unit } from 'models/schema';
 import { fetchPaginated } from 'api/fetch';
+import { callApi, previousPage, nextPage } from 'api/actions';
 
 const parse = (json) => normalize(json, [unit]);
 
-export const fetch = (affinities = {}, page = 0, orderBy = 'name', direction = 'ASC') => {
-   return {
-      [CALL_API]: {
-         tag: 'SPONSOR_UNITS',
-         chained: [CREATE_ABILITIES, CREATE_AFFINITIES, CREATE_RATED_UNITS],
-         endpoint,
-         page,
-         fetch: (url, params) => fetchPaginated(url, params, parse),
-         orderBy,
-         direction,
-         params: { affinities }
-      }
-   };
-};
+const tag = 'SPONSOR_UNITS';
+
+export const fetch = (affinities = {}, page = 0, orderBy = 'name', direction = 'ASC') => callApi({
+   tag,
+   chained: [CREATE_ABILITIES, CREATE_AFFINITIES, CREATE_RATED_UNITS],
+   endpoint,
+   page,
+   fetch: (url, params) => fetchPaginated(url, params, parse),
+   orderBy,
+   direction,
+   params: { affinities }
+});
+
+export const movePrevPage = () => previousPage({
+   tag,
+   storeSelector: (state) => state.pagination.ratedUnits,
+   fetch
+});
+
+export const moveNextPage = () => nextPage({
+   tag,
+   storeSelector: (state) => state.pagination.ratedUnits,
+   fetch
+});
