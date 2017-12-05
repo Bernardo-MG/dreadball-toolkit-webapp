2e7f9cfdec1ff2f7225855cdb838a3223f315051
Bernardo <programming@wandrell.com>
2017-12-05 18:13:53 +0100

    Removed old API action scripts

--- src/main/js/api/ActionTypes.js
+++
@@ -1,5 +0,0 @@
-export const CALL_API = 'CALL_API';
-
-export const REQUEST = 'REQUEST';
-export const REQUEST_SUCCESS = 'REQUEST_SUCCESS';
-export const REQUEST_FAILURE = 'REQUEST_FAILURE';
--- src/main/js/api/actions.js
+++
@@ -1,9 +0,0 @@
-import { CALL_API } from 'api/ActionTypes';
-
-export const callApi = (data = {}) => {
-   return {
-      [CALL_API]: {
-         ...data
-      }
-   };
-};
