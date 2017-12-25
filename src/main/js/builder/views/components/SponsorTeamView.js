2422c5dff9747acda204a548aabe3dac164df0ea
Bernardo <programming@wandrell.com>
2017-12-25 21:12:31 +0100

    Changed the builder view to a split view

--- src/main/js/builder/views/components/SponsorTeamView.js
+++ src/main/js/builder/views/components/SponsorTeamView.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Box from 'grommet/components/Box';
+import Sidebar from 'grommet/components/Sidebar';
+import Header from 'grommet/components/Header';
 import Heading from 'grommet/components/Heading';
-import Tab from 'grommet/components/Tab';
-import Tabs from 'grommet/components/Tabs';
+import Title from 'grommet/components/Title';
+import Menu from 'grommet/components/Menu';
+import Button from 'grommet/components/Button';
 
 import SponsorAffinitiesList from 'builder/affinities/containers/SponsorAffinitiesList';
 import SponsorUnitNameList from 'builder/units/containers/SponsorUnitNameList';
@@ -15,37 +18,81 @@ import SponsorNameInput from 'builder/sponsors/containers/SponsorNameInput';
 
 import BoundDbxTeamPlayersPanel from 'builder/views/containers/BoundDbxTeamPlayersPanel';
 
-const SponsorTeamView = () =>
-   <Tabs>
-      <Tab title='assets'>
-         <Box justify='center' align='center'>
-            <Box>
-               <SponsorTeamCost />
-            </Box>
-            <Box>
-               <SponsorNameInput />
-            </Box>
-            <Heading tag='h2'>assets</Heading>
-            <SponsorAssetsForm />
-            <Box direction='row' pad='small'>
-               <Box pad='small' size='medium'>
-                  <Box direction='row'>
-                     <Heading tag='h2'>affinities</Heading>
+class SponsorTeamView extends Component {
+
+   state = { showUnits: false };
+
+   showUnits = () => {
+      this.setState({ showUnits: true });
+   }
+
+   showAssets = () => {
+      this.setState({ showUnits: false });
+   }
+
+   render() {
+      let view = null;
+
+      if (this.state.showUnits) {
+         view =
+            <BoundDbxTeamPlayersPanel />;
+      } else {
+         view =
+            <Box justify='center' align='center' pad='medium'>
+               <Box>
+                  <SponsorTeamCost />
+               </Box>
+               <Box>
+                  <SponsorNameInput />
+               </Box>
+               <Heading tag='h2'>assets</Heading>
+               <SponsorAssetsForm />
+               <Box direction='row' pad='small'>
+                  <Box pad='small' size='medium'>
+                     <Box direction='row'>
+                        <Heading tag='h2'>affinities</Heading>
+                     </Box>
+                     <SponsorAffinitiesList />
                   </Box>
-                  <SponsorAffinitiesList />
+                  <Box pad='small' size='medium'>
+                     <Heading tag='h2'>players</Heading>
+                     <SponsorUnitNameList />
+                  </Box>
                </Box>
-               <Box pad='small' size='medium'>
-                  <Heading tag='h2'>players</Heading>
-                  <SponsorUnitNameList />
-               </Box>
+            </Box>;
+      }
+
+      return (
+         <Box direction='row' full={true} flex={true}>
+            <Box
+               justify='center'
+               align='center'
+               pad='medium'>
+               <Sidebar>
+                  <Header pad='medium'
+                     justify='between'>
+                     <Title>options</Title>
+                  </Header>
+                  <Box flex='grow'
+                     justify='start'>
+                     <Menu primary={true}>
+                        <Button onClick={this.showAssets}>assets</Button>
+                        <Button onClick={this.showUnits}>units</Button>
+                     </Menu>
+                  </Box>
+               </Sidebar>
+            </Box>
+            <Box
+               justify='center'
+               align='center'
+               pad='medium'
+               full={true} flex={true}>
+               { view }
             </Box>
          </Box>
-      </Tab>
-      <Tab title='players'>
-         <Box pad='medium' full={true}>
-            <BoundDbxTeamPlayersPanel />
-         </Box>
-      </Tab>
-   </Tabs>;
+      );
+   }
+}
+
 
 export default SponsorTeamView;
