3216913e7918ee7c106290062d0d017af9f6b9cd
Bernardo <programming@wandrell.com>
2018-01-14 14:18:29 +0100

    Reduced the size of the value fields

--- src/main/js/codex/components/UnitPanel.js
+++ src/main/js/codex/components/UnitPanel.js
@@ -6,12 +6,10 @@ import { injectIntl } from 'react-intl';
 
 import Box from 'grommet/components/Box';
 import Columns from 'grommet/components/Columns';
-import Heading from 'grommet/components/Heading';
 import Value from 'grommet/components/Value';
 
 import abilityMessages from 'i18n/ability';
 import unitMessages from 'i18n/unit';
-import unitNameMessages from 'i18n/unitName';
 import unitRoleMessages from 'i18n/role';
 
 class UnitPanel extends Component {
@@ -25,7 +23,7 @@ class UnitPanel extends Component {
       if (this.props.source.friendCost) {
          friendCost =
             <Box pad='small'>
-               <Value value={this.props.source.friendCost} label={this.props.intl.formatMessage(unitMessages.friend_cost)} />
+               <Value value={this.props.source.friendCost} label={this.props.intl.formatMessage(unitMessages.friend_cost)} size='small' />
             </Box>;
       } else {
          friendCost = null;
@@ -33,7 +31,7 @@ class UnitPanel extends Component {
       if (this.props.source.allyCost) {
          allyCost =
             <Box pad='small'>
-               <Value value={this.props.source.allyCost} label={this.props.intl.formatMessage(unitMessages.ally_cost)} />
+               <Value value={this.props.source.allyCost} label={this.props.intl.formatMessage(unitMessages.ally_cost)} size='small' />
             </Box>;
       } else {
          allyCost = null;
@@ -41,7 +39,7 @@ class UnitPanel extends Component {
       if (this.props.source.strangerCost) {
          strangerCost =
             <Box pad='small'>
-               <Value value={this.props.source.strangerCost} label={this.props.intl.formatMessage(unitMessages.stranger_cost)} />
+               <Value value={this.props.source.strangerCost} label={this.props.intl.formatMessage(unitMessages.stranger_cost)} size='small' />
             </Box>;
       } else {
          strangerCost = null;
@@ -49,7 +47,7 @@ class UnitPanel extends Component {
       if (this.props.source.cost) {
          cost =
             <Box pad='small'>
-               <Value value={this.props.source.cost} label={this.props.intl.formatMessage(unitMessages.cost)} />
+               <Value value={this.props.source.cost} label={this.props.intl.formatMessage(unitMessages.cost)} size='small' />
             </Box>;
       } else {
          cost = null;
@@ -65,25 +63,24 @@ class UnitPanel extends Component {
 
       return (
          <Box>
-            <Heading tag='h1'>{this.props.intl.formatMessage(unitNameMessages[this.props.source.name])}</Heading>
             <Box>{this.props.intl.formatMessage(unitMessages.role)}: {this.props.intl.formatMessage(unitRoleMessages[this.props.source.role])}</Box>
             <Box>{this.props.intl.formatMessage(unitMessages.abilities)}: {this.props.source.abilities.map((a) => this.props.intl.formatMessage(abilityMessages[a])).join(', ')}</Box>
             <Box>
                <Columns size='small'>
                   <Box pad='small'>
-                     <Value value={this.props.source.movement} label={this.props.intl.formatMessage(unitMessages.move)} />
+                     <Value value={this.props.source.movement} label={this.props.intl.formatMessage(unitMessages.move)} size='small' />
                   </Box>
                   <Box pad='small'>
-                     <Value value={this.props.source.strength} label={this.props.intl.formatMessage(unitMessages.strength)} />
+                     <Value value={this.props.source.strength} label={this.props.intl.formatMessage(unitMessages.strength)} size='small' />
                   </Box>
                   <Box pad='small'>
-                     <Value value={this.props.source.speed} label={this.props.intl.formatMessage(unitMessages.speed)} />
+                     <Value value={this.props.source.speed} label={this.props.intl.formatMessage(unitMessages.speed)} size='small' />
                   </Box>
                   <Box pad='small'>
-                     <Value value={this.props.source.skill} label={this.props.intl.formatMessage(unitMessages.skill)} />
+                     <Value value={this.props.source.skill} label={this.props.intl.formatMessage(unitMessages.skill)} size='small' />
                   </Box>
                   <Box pad='small'>
-                     <Value value={this.props.source.armor} label={this.props.intl.formatMessage(unitMessages.armor)} />
+                     <Value value={this.props.source.armor} label={this.props.intl.formatMessage(unitMessages.armor)} size='small' />
                   </Box>
                </Columns>
             </Box>
