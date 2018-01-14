40679f8a36834f2aae2c00c458cb19eaeb41fbfb
Bernardo <programming@wandrell.com>
2018-01-14 14:25:39 +0100

    Added title to the menu

---
+++ src/main/js/i18n/app.js
@@ -0,0 +1,10 @@
+import { defineMessages } from 'react-intl';
+
+const appMessages = defineMessages({
+   name: {
+      id: 'app.name',
+      defaultMessage: 'Dreadball Web Toolkit'
+   }
+});
+
+export default appMessages;
