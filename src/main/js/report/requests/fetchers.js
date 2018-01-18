487f100fa55df287fdcfd110b8a5287c7a395e12
Bernardo <programming@wandrell.com>
2018-01-18 22:50:47 +0100

    Added support for fetching files

--- src/main/js/report/requests/fetchers.js
+++ src/main/js/report/requests/fetchers.js
@@ -1,4 +1,4 @@
-import { Fetcher } from 'api/fetch';
+import { Fetcher } from 'api/fetch/file';
 import { teamReportEndpoint } from 'report/requests/endpoints';
 
 export const fetcherReport = new Fetcher(teamReportEndpoint);
