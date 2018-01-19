27e260e01ed787c8f5eeb2cc566a6430c054da33
Bernardo <programming@wandrell.com>
2018-01-19 20:01:57 +0100

    Formatted code

--- src/main/js/api/fetch/file.js
+++ src/main/js/api/fetch/file.js
@@ -1,36 +1,34 @@
 import request, { parse } from 'superagent';
 
-function showFile(blob) {
-   // It is necessary to create a new blob object with mime-type explicitly set
-   // otherwise only Chrome works like it should
-   const newBlob = new Blob([blob], { type: 'application/pdf' });
-
-   // IE doesn't allow using a blob object directly as link href
-   // instead it is necessary to use msSaveOrOpenBlob
-   if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveOrOpenBlob(newBlob);
-      return;
-   }
-
-   // For other browsers: 
-   // Create a link pointing to the ObjectURL containing the blob.
-   const data = window.URL.createObjectURL(newBlob);
-   const link = document.createElement('a');
-   link.href = data;
-   link.download = 'file.pdf';
-   link.click();
-   setTimeout(() => {
-      // For Firefox it is necessary to delay revoking the ObjectURL
-      window.URL.revokeObjectURL(data);
-   },
-   100
-   );
-}
-
 export const Fetcher = class {
 
    constructor(url) {
       this.url = url;
+      this.filename = 'team.pdf';
+   }
+
+   static showFile(blob, type, filename) {
+      // It is necessary to create a new blob object with mime-type explicitly set
+      // otherwise only Chrome works like it should
+      const newBlob = new Blob([blob], { type });
+
+      if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+         // IE doesn't allow using a blob object directly as link href
+         // instead it is necessary to use msSaveOrOpenBlob
+         window.navigator.msSaveOrOpenBlob(newBlob);
+      } else {
+         // For other browsers: 
+         // Create a link pointing to the ObjectURL containing the blob.
+         const data = window.URL.createObjectURL(newBlob);
+
+         const link = document.createElement('a');
+         link.href = data;
+         link.download = filename;
+         link.click();
+
+         // For Firefox it is necessary to delay revoking the ObjectURL
+         setTimeout(() => { window.URL.revokeObjectURL(data); }, 100);
+      }
    }
 
    fetch() {
@@ -39,7 +37,7 @@ export const Fetcher = class {
          .parse(parse.image)
          .then(
             (response) => {
-               showFile(response.body);
+               showFile(response.body, response.type, this.filename);
             },
             (error) => error.message || 'Request failed'
          );
