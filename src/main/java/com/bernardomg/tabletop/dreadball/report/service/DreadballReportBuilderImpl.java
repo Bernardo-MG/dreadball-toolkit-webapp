0abc4360da80abb48b8ef6b242a65ba07426e2eb
Bernardo <programming@wandrell.com>
2018-03-02 21:16:27 +0100

    Added more values to the PDF

--- src/main/java/com/bernardomg/tabletop/dreadball/report/service/DreadballReportBuilderImpl.java
+++ src/main/java/com/bernardomg/tabletop/dreadball/report/service/DreadballReportBuilderImpl.java
@@ -4,10 +4,12 @@ package com.bernardomg.tabletop.dreadball.report.service;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.springframework.stereotype.Service;
 
 import com.bernardomg.tabletop.dreadball.model.team.SponsorTeam;
+import com.google.common.collect.Iterables;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
@@ -53,10 +55,12 @@ public class DreadballReportBuilderImpl implements DreadballReportBuilder {
         final Paragraph general;
         final Paragraph assets;
         final Paragraph units;
+        final Paragraph affinities;
 
         general = getGeneralParagraph(team, paragraphFont);
         assets = getAssetsParagraph(team, paragraphFont);
         units = getUnitsParagraph(team, paragraphFont);
+        affinities = getAffinitiesParagraph(team, paragraphFont);
 
         final Paragraph header = new Paragraph(chunk);
 
@@ -66,8 +70,59 @@ public class DreadballReportBuilderImpl implements DreadballReportBuilder {
         document.add(linebreak);
         document.add(general);
         document.add(assets);
+        document.add(affinities);
         document.add(units);
         document.close();
+    }
+
+    private final Paragraph getAffinitiesParagraph(final SponsorTeam team,
+            final Font paragraphFont) {
+        final Paragraph paragraph;
+        final PdfPTable table;
+        final PdfPTable tableAdditional;
+
+        paragraph = new Paragraph();
+
+        paragraph.add(new Paragraph("affinities", paragraphFont));
+
+        table = new PdfPTable(1);
+        paragraph.add(table);
+
+        // Adds headers
+        Stream.of("affinity").forEach(columnTitle -> {
+            final PdfPCell header = new PdfPCell();
+            header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+            header.setBorderWidth(2);
+            header.setPhrase(new Phrase(columnTitle));
+            table.addCell(header);
+        });
+
+        team.getSponsor().getAffinityGroups().stream().forEach((affinity) -> {
+            table.addCell(affinity.getName());
+        });
+
+        if (!Iterables.isEmpty(team.getAdditionalAffinityGroups())) {
+            tableAdditional = new PdfPTable(1);
+            paragraph.add(tableAdditional);
+
+            // Adds headers
+            Stream.of("additional_affinity").forEach(columnTitle -> {
+                final PdfPCell header = new PdfPCell();
+                header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                header.setBorderWidth(2);
+                header.setPhrase(new Phrase(columnTitle));
+                tableAdditional.addCell(header);
+            });
+
+            StreamSupport
+                    .stream(team.getAdditionalAffinityGroups().spliterator(),
+                            false)
+                    .forEach((affinity) -> {
+                        tableAdditional.addCell(affinity.getName());
+                    });
+        }
+
+        return paragraph;
     }
 
     private final Paragraph getAssetsParagraph(final SponsorTeam team,
@@ -115,6 +170,7 @@ public class DreadballReportBuilderImpl implements DreadballReportBuilder {
     private final Paragraph getGeneralParagraph(final SponsorTeam team,
             final Font paragraphFont) {
         final Paragraph paragraph;
+        final Paragraph paraRankCost;
         final Paragraph paraRank;
         final Paragraph paraCost;
 
@@ -123,15 +179,23 @@ public class DreadballReportBuilderImpl implements DreadballReportBuilder {
         paragraph.add(new Paragraph("team", paragraphFont));
 
         paraRank = new Paragraph();
-        paraRank.add(new Chunk("rank_cost"));
+        paraRank.add(new Chunk("rank"));
         paraRank.setTabSettings(new TabSettings(56f));
         paraRank.add(Chunk.TABBING);
-        paraRank.add(new Chunk(String.valueOf(team.getRankCost())));
+        paraRank.add(new Chunk(String.valueOf(team.getBaseRank())));
 
         paragraph.add(paraRank);
 
+        paraRankCost = new Paragraph();
+        paraRankCost.add(new Chunk("rank_cost"));
+        paraRankCost.setTabSettings(new TabSettings(56f));
+        paraRankCost.add(Chunk.TABBING);
+        paraRankCost.add(new Chunk(String.valueOf(team.getRankCost())));
+
+        paragraph.add(paraRankCost);
+
         paraCost = new Paragraph();
-        paraCost.add(new Chunk("team_value"));
+        paraCost.add(new Chunk("total_cost"));
         paraCost.setTabSettings(new TabSettings(56f));
         paraCost.add(Chunk.TABBING);
         paraCost.add(new Chunk(String.valueOf(team.getTotalCost())));
