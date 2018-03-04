8533216870c9d51e9427b7a63ccdf6b1efa4934c
Bernardo <programming@wandrell.com>
2018-03-04 15:33:25 +0100

    Team units are now shown just like the units options

--- src/main/js/builder/units/containers/SponsorUnitNameList.js
+++ src/main/js/builder/units/containers/SponsorUnitNameList.js
@@ -1,35 +1,101 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import PropTypes from 'prop-types';
 
+import { connect } from 'react-redux';
+
+import { injectIntl } from 'react-intl';
+
+import Article from 'grommet/components/Article';
+import Box from 'grommet/components/Box';
+import Button from 'grommet/components/Button';
+import CircleInformationIcon from 'grommet/components/icons/base/CircleInformation';
+import Layer from 'grommet/components/Layer';
 import Table from 'grommet/components/Table';
+import TableHeader from 'grommet/components/TableHeader';
 import TableRow from 'grommet/components/TableRow';
 
 import RemoveUnitButton from 'builder/units/containers/buttons/RemoveUnitButton';
+import UnitPanel from 'codex/components/UnitPanel';
 
-import { connect } from 'react-redux';
+import labelMessages from 'i18n/label';
+import unitMessages from 'i18n/unit';
+import unitNameMessages from 'i18n/unitName';
+import unitRoleMessages from 'i18n/role';
 
-import { selectUnits } from 'builder/units/selectors';
+import { selectSponsorRatedUnits } from 'builder/units/selectors';
 
-const SponsorUnitNameList = (props) =>
-   <Table>
-      <tbody>
-         { props.sponsorUnits.map((unit, i) =>
-            <TableRow key={i}>
-               <td><RemoveUnitButton unit={unit} /></td>
-               <td>{unit}</td>
-            </TableRow>
-         )}
-      </tbody>
-   </Table>;
+class SponsorUnitNameList extends Component {
+
+   constructor(props) {
+      super(props);
+      this.state = { selection: undefined };
+   }
+
+   _onSelect(selection) {
+      this.setState({ selection });
+   }
+
+   _onDeselect() {
+      this.setState({ selection: undefined });
+   }
+
+   render() {
+      const { selection } = this.state;
+      let detailsLayer;
+
+      const select = this._onSelect.bind(this);
+      const deselect = this._onDeselect.bind(this);
+
+      if (selection) {
+         detailsLayer = (
+            <Layer closer={true} onClose={deselect}>
+               <Article size='large'>
+                  <UnitPanel source={selection} />
+               </Article>
+            </Layer>
+         );
+      } else {
+         detailsLayer = undefined;
+      }
+
+      const headers = [];
+      headers.push(this.props.intl.formatMessage(labelMessages.add));
+      headers.push(this.props.intl.formatMessage(unitMessages.name));
+      headers.push(this.props.intl.formatMessage(unitMessages.role));
+      headers.push(this.props.intl.formatMessage(unitMessages.cost));
+      headers.push(this.props.intl.formatMessage(labelMessages.info));
+
+      return (
+         <Box>
+            {detailsLayer}
+            <Table>
+               <TableHeader labels={headers} />
+               <tbody>
+                  { this.props.source.map((unit, i) =>
+                     <TableRow key={i}>
+                        <td><RemoveUnitButton unit={unit.templateName} /></td>
+                        <td>{this.props.intl.formatMessage(unitNameMessages[unit.name])}</td>
+                        <td>{this.props.intl.formatMessage(unitRoleMessages[unit.role])}</td>
+                        <td>{unit.cost}</td>
+                        <td><Button onClick={() => select(unit)} icon={<CircleInformationIcon/>} /></td>
+                     </TableRow>
+                  )}
+               </tbody>
+            </Table>
+         </Box>
+      );
+   }
+}
 
 SponsorUnitNameList.propTypes = {
-   sponsorUnits: PropTypes.array.isRequired
+   source: PropTypes.array.isRequired,
+   intl: PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => {
    return {
-      sponsorUnits: selectUnits(state)
+      source: selectSponsorRatedUnits(state)
    };
 };
 
@@ -37,7 +103,7 @@ const mapDispatchToProps = () => {
    return {};
 };
 
-export default connect(
+export default injectIntl(connect(
    mapStateToProps,
    mapDispatchToProps
-)(SponsorUnitNameList);
+)(SponsorUnitNameList));
