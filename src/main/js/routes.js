7751fca13ba3bd7cc60f36dd5bc987399d267600
Bernardo <programming@wandrell.com>
2018-03-08 21:34:21 +0100

    Removed home view

--- src/main/js/routes.js
+++ src/main/js/routes.js
@@ -3,10 +3,9 @@ import { Route, IndexRoute } from 'react-router';
 import PlayersScrollPanel from 'codex/containers/PlayersScrollPanel';
 import ContainerDbxTeamBuilder from 'builder/views/containers/ContainerDbxTeamBuilder';
 import BaseLayout from 'components/layout/BaseLayout';
-import Home from 'components/views/Home';
 
 export default <Route path="/" component={BaseLayout}>
-   <IndexRoute component={Home}/>
+   <IndexRoute component={ContainerDbxTeamBuilder}/>
    <Route path="/players" activeClassName="dbxPlayersLink" component={PlayersScrollPanel}/>
    <Route path="/dbx" activeClassName="dbxTeamBuilderLink" component={ContainerDbxTeamBuilder}/>
 </Route>;
