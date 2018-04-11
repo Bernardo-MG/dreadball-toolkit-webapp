04ea9002dc06f95edabbd27b19c311bf860db4c9
Bernardo <programming@wandrell.com>
2018-04-11 21:30:08 +0200

    Added Postgres support

--- .scripts/create-test-databases.sh
+++ .scripts/create-test-databases.sh
@@ -2,13 +2,16 @@
 #
 # Creates the test databases.
 #
-# This script requires a MySQL service, with the default super user, where the test 
-# databases will be created.
+# This script requires a MySQL and a PostgreSQL services, with the default super user,
+# where the test databases will be created.
 
 set -o nounset
 set -e
 
 # MySQL tables
 mysql -e "create database IF NOT EXISTS dreadball_toolkit;" -uroot
+
+# PostgreSQL tables
+psql -c 'create database dreadball_toolkit;' -U postgres
 
 exit 0
