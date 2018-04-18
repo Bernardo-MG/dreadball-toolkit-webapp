54e63347b7d87f06787129facd6b889fd6dcc0b8
Bernardo <programming@wandrell.com>
2018-04-18 22:41:23 +0200

    Fixed imports

--- src/main/js/builder/reducers/index.js
+++ src/main/js/builder/reducers/index.js
@@ -1,9 +1,8 @@
-import * as ActionTypes from 'builder/actions/actionTypes';
 import { combineReducers } from 'redux';
 import assets from 'builder/assets/reducers';
 import affinities from 'builder/affinities/reducers';
 import sponsor from 'builder/sponsors/reducers';
 
-const dbxBuilderReducer = combineReducers({ sponsor, assets, affinities, sponsor });
+const dbxBuilderReducer = combineReducers({ sponsor, assets, affinities });
 
 export default dbxBuilderReducer;
