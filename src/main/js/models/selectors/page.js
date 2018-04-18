285001caed7a33a23860e0848492b12cb5b2d7e3
Bernardo <programming@wandrell.com>
2018-04-18 21:48:40 +0200

    Reduced number of requests

--- src/main/js/models/selectors/page.js
+++ src/main/js/models/selectors/page.js
@@ -1,16 +1,20 @@
 
-export const selectCurrentRatedPlayerPage = (state) => state.pagination.ratedPlayers.page;
-
 export const selectCurrentPlayerPage = (state) => state.pagination.players.page;
 
 export const selectLastPlayerPage = (state) => state.pagination.players.last;
 
 export const selectIsFetchingPlayer = (state) => state.pagination.players.isFetching;
 
-export const selectLastRatedPlayerPage = (state) => state.pagination.ratedPlayers.last;
-
 export const selectCurrentPlayerPageVisual = (state) => state.pagination.players.page + 1;
 
 export const selectTotalPlayerPages = (state) => state.pagination.players.totalPages;
 
 export const selectCanLoadPlayer = (state) => !selectLastPlayerPage(state) && !selectIsFetchingPlayer(state);
+
+export const selectCurrentRatedPlayerPage = (state) => state.pagination.ratedPlayers.page;
+
+export const selectLastRatedPlayerPage = (state) => state.pagination.ratedPlayers.last;
+
+export const selectIsFetchingRatedPlayer = (state) => state.pagination.players.isFetching;
+
+export const selectCanLoadRatedPlayer = (state) => !selectLastRatedPlayerPage(state) && !selectIsFetchingRatedPlayer(state);
