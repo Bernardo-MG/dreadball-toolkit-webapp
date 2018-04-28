e29fd8dc60da3685c94e8efcd1dce2f0c8936578
Bernardo <programming@wandrell.com>
2018-04-28 14:32:15 +0200

    Fixed typo

--- src/main/js/models/sagas/affinityPlayers/index.js
+++ src/main/js/models/sagas/affinityPlayers/index.js
@@ -3,7 +3,7 @@ import * as types from 'models/actions/actionTypes';
 import { requestCurrentPage, requestNextPage } from 'models/sagas/affinityPlayers/requests';
 import { build } from 'models/sagas/affinityPlayers/builders';
 
-export const affinityPlayersagas = [
+export const affinityPlayerSagas = [
    takeLatest(types.REQUEST_TEAM_PLAYERS, requestCurrentPage),
    takeLatest(types.REQUEST_SUCCESS_TEAM_PLAYERS, build),
    takeLatest(types.CHANGE_PAGE_NEXT_TEAM_PLAYERS, requestNextPage)
