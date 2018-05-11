c6cd7d224aa776055b7fd1c48d18fe5d4f8ab974
Bernardo <programming@wandrell.com>
2018-05-12 00:10:06 +0200

    Updated dependencies

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ const common = require('./webpack.common.js');
 process.traceDeprecation = true;
 
 module.exports = merge(common, {
-   devtool: 'inline-source-map',
+   devtool: 'cheap-module-eval-source-map',
    plugins: [
       new webpack.NamedModulesPlugin(),
       new webpack.HotModuleReplacementPlugin(),
