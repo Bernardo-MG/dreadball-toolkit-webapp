38cba5bf7ba18e9a0c60f3c09afb59881efada78
Bernardo <programming@wandrell.com>
2018-05-11 16:31:09 +0200

    Added tests and fixed setting the sponsor name

---
+++ src/main/js/__tests__/builder/sponsors/reducers/sponsorNameReducerTest.js
@@ -0,0 +1,25 @@
+import team from 'builder/sponsors/reducers';
+import * as types from 'builder/actions/actionTypes';
+
+describe('Sponsor reducer', () => {
+   it('sets sponsor name', () => {
+      expect(
+         team({}, {
+            type: types.SET_SPONSOR_NAME,
+            payload: 'name'
+         })
+      ).toEqual(
+            { 'sponsorName': 'name' }
+      )
+   }),
+   it('overwrites sponsor name', () => {
+      expect(
+         team({ 'sponsorName': 'name' }, {
+            type: types.SET_SPONSOR_NAME,
+            payload: ''
+         })
+      ).toEqual(
+            { 'sponsorName': '' }
+      )
+   })
+});
