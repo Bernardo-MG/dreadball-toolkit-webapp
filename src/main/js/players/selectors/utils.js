22d6449bfed192bd3c55512b7331c4411b3928f6
Bernardo <programming@wandrell.com>
2018-05-20 16:11:39 +0200

    Cleaned up code

--- src/main/js/players/selectors/utils.js
+++ src/main/js/players/selectors/utils.js
@@ -1,18 +1,29 @@
 
-export const filterByKeys = (data, keys) => {
+/**
+ * Returns all the values from the map received in the list.
+ * 
+ * @param map map to filter
+ * @param keys keys for filtering
+ */
+export const filterByKeys = (map, keys) => {
    const result = [];
 
    keys.forEach((k) => {
-      if (k in data) {
-         result.push(data[k]);
+      if (k in map) {
+         result.push(map[k]);
       }
    });
 
    return result;
 };
 
-export const selectAllValues = (data) => {
-   const keys = Object.keys(data);
+/**
+ * Returns all the values in the map.
+ * 
+ * @param map map to acquire the values from
+ */
+export const selectAllValues = (map) => {
+   const keys = Object.keys(map);
 
-   return filterByKeys(data, keys);
+   return filterByKeys(map, keys);
 };
