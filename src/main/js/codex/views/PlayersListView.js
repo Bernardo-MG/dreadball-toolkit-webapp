2f8c0fbf8d22d8041c2563bc030ec1d6ff610e0a
Bernardo <programming@wandrell.com>
2018-05-27 16:41:12 +0200

    The initial data is loaded in the view

--- src/main/js/codex/views/PlayersListView.js
+++ src/main/js/codex/views/PlayersListView.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import PropTypes from 'prop-types';
 
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
 import { injectIntl } from 'react-intl';
+
+import { fetch } from 'players/actions/players';
 
 import SimpleView from 'views/containers/SimpleView';
 import PlayersScrollPanel from 'codex/containers/PlayersScrollPanel';
 
 import titleMessages from 'i18n/title';
 
-const PlayersListView = (props) =>
-   <SimpleView title={props.intl.formatMessage(titleMessages.playersCodex)}>
-      <PlayersScrollPanel />
-   </SimpleView>;
+/**
+ * View for the units codex.
+ * 
+ * It also takes care of loading the initial data.
+ */
+class PlayersListView extends Component {
+
+   componentDidMount() {
+      this.props.load();
+   }
+
+   render() {
+      return (
+         <SimpleView title={this.props.intl.formatMessage(titleMessages.playersCodex)}>
+            <PlayersScrollPanel />
+         </SimpleView>
+      );
+   }
+}
 
 PlayersListView.propTypes = {
-   intl: PropTypes.object.isRequired
+   intl: PropTypes.object.isRequired,
+   load: PropTypes.func.isRequired
 };
 
-export default injectIntl(PlayersListView);
+const mapStateToProps = () => {
+   return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+   return {
+      load: bindActionCreators(fetch, dispatch)
+   };
+};
+
+export default injectIntl(connect(
+   mapStateToProps,
+   mapDispatchToProps
+)(PlayersListView));
