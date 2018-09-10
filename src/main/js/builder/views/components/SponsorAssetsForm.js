08e22d452cf2a6d351d68e808adf501b3ad636b7
Bernardo <programming@wandrell.com>
2018-09-10 22:34:41 +0200

    Using columns to group the inputs

--- src/main/js/builder/views/components/SponsorAssetsForm.js
+++ src/main/js/builder/views/components/SponsorAssetsForm.js
@@ -4,8 +4,7 @@ import PropTypes from 'prop-types';
 
 import { injectIntl } from 'react-intl';
 
-import Box from 'grommet/components/Box';
-import Form from 'grommet/components/Form';
+import Columns from 'grommet/components/Columns';
 import FormField from 'grommet/components/FormField';
 
 import CheerleadersInput from 'builder/assets/containers/CheerleadersInput';
@@ -18,32 +17,26 @@ import WagerInput from 'builder/assets/containers/WagerInput';
 import teamBuilderMessages from 'i18n/teamBuilder';
 
 const SponsorAssetsForm = (props) =>
-   <Form>
-      <Box direction='row'>
-         <FormField label={props.intl.formatMessage(teamBuilderMessages.coaching_dice)}>
-            <CoachingDiceInput id='coaching_dice' name='coaching_dice' min={0} max={100}/>
-         </FormField>
-         <FormField label={props.intl.formatMessage(teamBuilderMessages.special_move_cards)}>
-            <SpecialMoveCardInput id='special_move_cards' name='special_move_cards' min={0} max={100}/>
-         </FormField>
-      </Box>
-      <Box direction='row'>
-         <FormField label={props.intl.formatMessage(teamBuilderMessages.nasty_surprise_cards)}>
-            <NastySurpriseCardInput id='nasty_surprise_cards' name='nasty_surprise_cards' min={0} max={100}/>
-         </FormField>
-         <FormField label={props.intl.formatMessage(teamBuilderMessages.wagers)}>
-            <WagerInput id='wagers' name='wagers' min={0} max={100}/>
-         </FormField>
-      </Box>
-      <Box direction='row'>
-         <FormField label={props.intl.formatMessage(teamBuilderMessages.medibots)}>
-            <MediBotInput id='medibots' name='medibots' min={0} max={100}/>
-         </FormField>
-         <FormField label={props.intl.formatMessage(teamBuilderMessages.cheerleaders)}>
-            <CheerleadersInput id='cheerleaders' name='cheerleaders' min={0} max={100}/>
-         </FormField>
-      </Box>
-   </Form>;
+   <Columns>
+      <FormField label={props.intl.formatMessage(teamBuilderMessages.coaching_dice)}>
+         <CoachingDiceInput id='coaching_dice' name='coaching_dice' min={0} max={100}/>
+      </FormField>
+      <FormField label={props.intl.formatMessage(teamBuilderMessages.special_move_cards)}>
+         <SpecialMoveCardInput id='special_move_cards' name='special_move_cards' min={0} max={100}/>
+      </FormField>
+      <FormField label={props.intl.formatMessage(teamBuilderMessages.nasty_surprise_cards)}>
+         <NastySurpriseCardInput id='nasty_surprise_cards' name='nasty_surprise_cards' min={0} max={100}/>
+      </FormField>
+      <FormField label={props.intl.formatMessage(teamBuilderMessages.wagers)}>
+         <WagerInput id='wagers' name='wagers' min={0} max={100}/>
+      </FormField>
+      <FormField label={props.intl.formatMessage(teamBuilderMessages.medibots)}>
+         <MediBotInput id='medibots' name='medibots' min={0} max={100}/>
+      </FormField>
+      <FormField label={props.intl.formatMessage(teamBuilderMessages.cheerleaders)}>
+         <CheerleadersInput id='cheerleaders' name='cheerleaders' min={0} max={100}/>
+      </FormField>
+   </Columns>;
 
 SponsorAssetsForm.propTypes = {
    intl: PropTypes.object.isRequired
