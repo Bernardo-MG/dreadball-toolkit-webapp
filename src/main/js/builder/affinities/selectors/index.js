2346cac71b5d32a022d66c55fbffed9f15d5762a
Bernardo <programming@wandrell.com>
2018-09-18 17:46:26 +0200

    Fixed finished check

--- src/main/js/builder/affinities/selectors/index.js
+++ src/main/js/builder/affinities/selectors/index.js
@@ -3,4 +3,4 @@ export const selectChosenAffinities = (state) => state.builder.affinities.chosen
 
 export const selectAffinityOptions = (state) => state.builder.affinities.options;
 
-export const selectAllAffinitiesChosen = (state) => selectChosenAffinities(state).length === selectAffinityOptions(state).length;
+export const selectAllAffinitiesChosen = (state) => selectChosenAffinities(state).filter((x) => x).length >= selectAffinityOptions(state).length;
