954da7d2de726c2adb5fb28f0bc40a0da2c2f6de
Bernardo <programming@wandrell.com>
2018-09-19 18:50:36 +0200

    Nulls are always removed from selected affinities

--- src/main/js/builder/affinities/selectors/index.js
+++ src/main/js/builder/affinities/selectors/index.js
@@ -1,6 +1,6 @@
 
-export const selectChosenAffinities = (state) => state.builder.affinities.chosen;
+export const selectChosenAffinities = (state) => state.builder.affinities.chosen.filter((x) => x);
 
 export const selectAffinityOptions = (state) => state.builder.affinities.options;
 
-export const selectAllAffinitiesChosen = (state) => selectChosenAffinities(state).filter((x) => x).length >= selectAffinityOptions(state).length;
+export const selectAllAffinitiesChosen = (state) => selectChosenAffinities(state).length >= selectAffinityOptions(state).length;
