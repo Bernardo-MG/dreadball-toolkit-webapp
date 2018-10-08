6fb8db0c654ee2dfe06524d40ed06a6c42fc174a
Bernardo <programming@wandrell.com>
2018-10-08 22:25:13 +0200

    Moved requests to the new package

---
+++ src/main/js/builder/requests/validations/actions/index.js
@@ -0,0 +1,15 @@
+import * as types from 'builder/requests/validations/actions/actionTypes';
+
+export const validateTeamSuccess = (payload) => {
+   return {
+      type: types.REQUEST_SUCCESS_TEAM_VALIDATION,
+      payload
+   };
+};
+
+export const validateTeamFailure = (payload) => {
+   return {
+      type: types.REQUEST_FAILURE_TEAM_VALIDATION,
+      payload
+   };
+};
